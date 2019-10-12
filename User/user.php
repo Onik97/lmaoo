cea1507fe7f70b5865adf31bc5b556848cefafb4
Onik Noor <18009030+OnikNoor97@users.noreply.github.com>
2019-10-12 16:56:42 +0100

    Revert "Fixes Merge"
    This reverts commit 366d086ab3bf9485a435a46b242c8a2ba927e411.

--- User/user.php
+++ User/user.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 class user
 {
@@ -66,4 +67,86 @@ class user
 		return $this->surname;
 	}
 }
+=======
+<?php
+class user
+{
+	private $id;
+	private $username;
+	private $password;
+	private $forename;
+	private $surname;
+	private $level;
+
+	function __construct($id, $forename, $surname, $username, $password, $level)
+	{
+		$this->id = $id;
+		$this->forename = $forename;
+		$this->surname = $surname;
+		$this->username = $username;
+		$this->password = $password;
+		$this->level = $level;
+	}
+
+	public function setId($id)
+	{
+		$this->id = $id;
+	}
+
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	public function setUsername($username)
+	{
+		$this->username = $username;
+	}
+// 
+	public function getUsername()
+	{
+		return $this->username;
+	}
+
+	public function setPassword($password)
+	{
+		$this->password = $password;
+	}
+
+	public function getPassword()
+	{
+		return $this->password;
+	}
+
+	public function setForename($forename)
+	{
+		$this->forename = $forename;
+	}
+
+	public function getForename()
+	{
+		return $this->forename;
+	}
+
+	public function setSurname($surname)
+	{
+		$this->surname = $surname;
+	}
+
+	public function getSurname()
+	{
+		return $this->surname;
+	}
+
+	public function setLevel($level)
+	{
+		$this->level = $level;
+	}
+
+	public function getLevel()
+	{
+		return $this->level;
+	}
+}
+>>>>>>> 33f64ffd8dcfb4211b9cff3484e2b4c54d185db2
 ?>
