60a206f05add8d60cd3ec34f8f009c77f8a1325e
MarcusBeddall <56123521+MarcusBeddall@users.noreply.github.com>
2019-10-12 18:12:42 +0100

    Fixed css

--- User/index.php
+++ User/index.php
@@ -5,6 +5,8 @@ session_start();
 <html>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" href="UserPage.css">
+<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <title>Login</title>
 <head>
 
@@ -50,8 +52,7 @@ session_start();
 </nav>
 	</head>
 <body>
-         <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 
 <div class="top-buffer">
 	<div class="container">
