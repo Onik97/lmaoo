22634ccf3cf601b580df19a79fe402535c121d09
= <=>
2019-10-13 13:17:14 +0100

    Fixed the Css rel links inside the About page, Register page and Login page

--- About/index.php
+++ About/index.php
@@ -4,7 +4,7 @@ session_start();
 <!DOCTYPE html>
 <html>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="AboutPage.css">
+<link rel="stylesheet" href="../Css/AboutPage.css">
 <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <title>About</title>
--- User/register.php
+++ User/register.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html>
 <html>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="RegisterPage.css">
+<link rel="stylesheet" href="../Css/RegisterPage.css">
  <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <title>Register Page</title>
