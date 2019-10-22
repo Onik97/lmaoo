96edc617cade7f89e48bd961a82ea263c23b15b7
Onik <O_Noor@live.co.uk>
2019-10-22 20:32:28 +0100

    Tidied Admin Page

--- User/admin.php
+++ User/admin.php
@@ -50,7 +50,6 @@
             } ?>
                 </table>
             </div>    
-    <!--  -->
     <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
