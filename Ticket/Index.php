ea5b4b795af804b7ba7a9709db3fc31ce7823964
Onik <O_Noor@live.co.uk>
2019-10-24 22:20:28 +0100

    Split whole Ticket Page to Seperate Files

--- Ticket/Index.php
+++ Ticket/Index.php
@@ -13,50 +13,38 @@ session_start();
 <?php include("../Global/navBar.php"); ?>
 <body>
 	<div ID="ticketContainer">
-    		<div ID="ticketBrief">
-    		<p ID="ticketBrief1"> Project Name:            '' </p>
-			<p ID="ticketBrief2"> Ticket ID:               '' </p> 
-			<p ID="ticketBrief3"> Ticket Name:             '' </p>
- 
+			
+			<div ID="ticketBrief">
+			<?php include("brief.php"); ?>	
     		</div>
 
     		<div ID="ticketPeople">
-    		  <p ID="ticketPeople1"> Reporter:  '[]' </p>
-			  <p ID="ticketPeople2"> Assignee:  '[]' </p>
-			  <a ID="ticketPeople3" href=""> Click here to assign it to yourself </a>
+			<?php include("people.php"); ?>	
     		</div>
 
     		<div ID="ticketDetails">
-    		  <h1 align=""> Details </h1>
+			<?php include("details.php"); ?>	
     		</div>
 
     		<div ID="ticketDate"> 
-    		  <h1> Dates </h1>
-                
-                <div ID="dates1">   
-                <p>Created Date:</p> <br>
-                <p>Start Date:</p> <br>
-                </div>
-    		    <div ID="dates2">
-                <p>Updated Date:</p> <br>
-                <p>Deadline:</p> <br> 
-                </div>
-
+			<?php include("dates.php"); ?>	
             </div>
 
             <div ID="detailsSubBug">
-            <h1>Sub Bug</h1>
+            <?php include("subBug.php"); ?>	
             </div>
 
     		<div ID="ticketMessages">
-    		<h1> Dev Messages </h1>
+    		<?php include("messages.php"); ?>	
     		</div>
 	</div>
+		  
 		  <div ID="ticketCreate">
-		      <h1> Comment Creator <h1>
+		  <?php include("createComment.php"); ?>	
 		  </div>
+		  
 		  <div ID="ticketComments">
-		      <h1> Comment Section </h1>
+		  <?php include("viewComments.php"); ?>	
 		  </div>
 		  
 		  <?php include("../Global/editUserModal.php"); ?>
