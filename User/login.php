e86a7255abc547d8f17acc9ee7406334b1914dbb
Onik <O_Noor@live.co.uk>
2019-11-11 11:46:16 +0000

    Tidied and Required both fields to be input

--- User/login.php
+++ User/login.php
@@ -16,10 +16,9 @@ if(isset($_SESSION['userLoggedIn'])){header("Location: ../Ticket/index.php");}
 	<div class="container">
 		<form action="userController.php" method='POST'>
 			Username:<br>
-		<input type="text" name="username">
-		<br>
+		<input type="text" name="username" required> <br>
 			Password:<br>
-		<input type="password" name="password">
+		<input type="password" name="password" required>
 		<input type="hidden" name="function" value="login">
 		<input class="one" type="submit" value="Submit"> <br><br>
 		<a href="../User/register.php">Not Registered? Click here!</a>
