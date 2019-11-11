ee2aecf730510ef0a6dba5af16bee4e83c88032b
Onik <O_Noor@live.co.uk>
2019-11-11 11:46:43 +0000

    Field checker removed & cleaned up code

--- Javascript/User/passwordchecker.js
+++ Javascript/User/passwordchecker.js
@@ -1,28 +1,15 @@
-           // Function to check Whether both passwords 
-            // is same or not. 
-            function checkPassword(form) { 
-                password1 = form.password1.value; 
-                password2 = form.password2.value; 
-  
-                // If password not entered 
-                if (password1 == '') 
-                    alert ("Please enter Password");
-					
-                      
-                // If confirm password not entered 
-                else if (password2 == '') 
-                    alert ("Please enter confirm password");
-					
-                      
-                // If Not same return False.     
-                else if (password1 != password2) { 
+function checkPassword(form) 
+{ 
+    password1 = form.password1.value; 
+    password2 = form.password2.value; 
+
+                if (password1 != password2) 
+                { 
                     alert ("\nPassword did not match: Please try again...") 
                     return false; 
-                } 
-  
-                // If same return True. 
-                else{ 
-                    alert("Password Match: Welcome to Lmaoo Project!") 
+                }
+                else
+                {
                     return true; 
                 } 
             } 
