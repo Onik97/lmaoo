f3e8d26234cef5e206f3edbdb76db57c808b9637
= <=>
2019-11-29 00:43:54 +0000

    fixed the navbar so edit user now has a clickonme icon when mouse is hovering over it, also added a modal.php which currently does not work, ignore this for now.

---
+++ Global/CreateCommentModal.php
@@ -0,0 +1,34 @@
+<?php if (!isset($userLoggedIn))  { return; } else { ?>
+            <div class="modal fade" id="view-modal2" tabindex="-1" role="dialog" aria-labelledby="view-modal2" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                     <h5 class="modal-title" id="exampleModalLongTitle">Account Details</h5>
+                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                    </div>
+                    <form action="../User/userController.php" method="POST">
+                   <div class="modal-body">
+                    <div class="form-group">
+                        <label>Forename</label>
+                        <input class="form-control" value=<?php echo $userLoggedIn->getForename(); ?>  name="editForename" >
+                    </div>
+                    <div class="form-group">
+                        <label>Surname</label>
+                        <input class="form-control" value=<?php echo $userLoggedIn->getSurname(); ?>  name="editSurname">
+                    </div>
+                    <div class="form-group">
+                        <label>Username</label>
+                        <input class="form-control" value=<?php echo $userLoggedIn->getUsername(); ?> name="editUsername">
+                    </div>
+                    </div>
+                    <input type="hidden" name="function" value="update">
+                 <div class="modal-footer">
+                 <input class="btn btn-primary" type="submit" value="Save Changes">
+                </div>
+                </form>
+                 </div>
+            </div>
+</div> <?php } ?>
+        </div>
