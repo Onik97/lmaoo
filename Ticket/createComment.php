6c7888ded217d64795d8d486d0380b941a902afe
Onik <O_Noor@live.co.uk>
2019-11-30 14:00:37 +0000

    Started Comment Creation

--- Ticket/createComment.php
+++ Ticket/createComment.php
@@ -1,13 +1,11 @@
-<!-- include libraries(jQuery, bootstrap) -->
 <h1> Create Comment </h1>
-<script type="text/javascript" src="../Script/createComment.js"></script>
-<link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.css" rel="stylesheet">
-<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script> 
-<script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script> 
-
-<!-- include summernote css/js -->
+<script> 
+var userId = "<?php echo $userLoggedIn->getId(); ?>"; 
+var ticketId = "<?php echo $ticketId; ?>";
+</script>
+<script type="text/javascript" src="../Script/commentController.js"></script>
 <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.css" rel="stylesheet">
 <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.js"></script>
 
-    <textarea id="createComment" class=createComment name="editordata" required></textarea>
-    <button onclick="saveComment()">Save</button>
+<textarea id="createComment" class=createComment name="editordata" required></textarea>
+<button onclick="saveComment()">Save</button>
