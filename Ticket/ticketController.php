a82bb5f38ba7ed68040365f55abf7558f9e1230f
Onik <O_Noor@live.co.uk>
2019-12-04 21:02:52 +0000

    Create and load comments works
    Load comment only works on the console

--- Ticket/ticketController.php
+++ Ticket/ticketController.php
@@ -1,11 +1,24 @@
 <?php
-
+require('../connection.php');
 $function = $_POST['function'];
-$section = $_POST['section'];
 
-function createComments($userId, $projectId)
+if ($function == "createComment")
 {
-    
+    createComment($_POST['commentContent'], $_POST['ticketId'], $_POST['userId']);
+}
+else if ($function == "loadComments")
+{
+    echo json_encode(loadComments($_POST['ticketId']));
+}
+
+
+function createComment($commentContent, $ticketId, $userId)
+{
+    $pdo = logindb('user', 'pass');
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    $stmt = $pdo->prepare("INSERT INTO comment (commentId, commentContent, ticketId, userId) VALUES (null, ?, ?, ?)");
+    $stmt->execute([$commentContent, $ticketId, $userId]);
+    echo "Success"; // Using echo for XMLHttpRequest
 }
 
 function createBugs($userId, $projectId)
@@ -25,7 +38,14 @@ function deleteBugs($bugId)
 
 function loadComments($ticketId)
 {
-
+    $pdo = logindb('user', 'pass');
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    $stmt = $pdo->prepare("SELECT comment.commentId, comment.commentContent, user.userId, user.forename, user.surname 
+                           FROM comment INNER JOIN user on user.userId = comment.userId
+                           WHERE comment.ticketId = ?");
+    $stmt->execute([$ticketId]);
+    $comments = $stmt->fetchAll();
+    return $comments;
 }
 
 function loadBugs($ticketId)
