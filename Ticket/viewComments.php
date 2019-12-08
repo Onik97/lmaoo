b5cfad3bbe3c56a18afcd5d69ec379f2d9507b39
= <=>
2019-12-08 19:59:55 +0000

    Added new options to CommentModal, renamed EditCommentmodal to CommentModal

--- Ticket/viewComments.php
+++ Ticket/viewComments.php
@@ -4,12 +4,13 @@ var ticketId = "<?php echo $ticketId; ?>";
 </script>
 <h1> Comment Section </h1>
 <button onclick="loadComments()">Load Comments - Check Console (F12 - Console) </button>
+<script type="text/javascript" src="../Script/CommentModal.js"></script>
 
 <div id="commentList">
 	<img class="CommentImages" src="../Images/paperclip.png"></img>
-	<img class="CommentImages" src="../Images/delete.png"></img>
-	<img class="CommentImages" src="../Images/edit.png" data-toggle="modal" data-target="#editCommentModal" role="button"></img>
+	<img class="CommentImages" src="../Images/delete.png" data-toggle="modal" data-target="#CommentModal" onclick="deleteComment()" role="button"></img>
+	<img class="CommentImages" src="../Images/edit.png" data-toggle="modal" data-target="#CommentModal" onclick="editComment()" role="button"></img>
     <p>$Username Added a comment Username </p>
 	<p>$Comment Content</p>
 </div>
-<?php include("editCommentModal.php"); ?>
+<?php include("CommentModal.php"); ?>
