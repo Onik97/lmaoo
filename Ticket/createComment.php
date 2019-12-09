c1a2a280f6d72b18d595bcf9e56c0337f183e13b
= <=>
2019-12-09 20:54:55 +0000

    Added warning to Createcomment when text is to long or there is no text at all

--- Ticket/createComment.php
+++ Ticket/createComment.php
@@ -1,4 +1,6 @@
 <h1> Create Comment </h1>
+<div ID="notenoughchars" class="alert alert-warning " role="alert" hidden>The text field is empty!</div>
+<div ID="manychars" class="alert alert-warning " role="alert" hidden>Text is to long inside text field!</div>
 <script> 
 var userId = "<?php echo $userLoggedIn->getId(); ?>"; 
 var ticketId = "<?php echo $ticketId; ?>";
