dcc332cb72c30e08de31adf009b2ebb28a32c7a1
Onik <O_Noor@live.co.uk>
2019-12-18 21:13:56 +0000

    Front End - Edit Comment works. Prepared Delete comments for Lewis

--- Script/commentController.js
+++ Script/commentController.js
@@ -12,17 +12,6 @@ $(document).ready(function()
         ['para', ['ul', 'ol']],
       ]
   });
-
-  $('.editComment').summernote
-  ({
-      height: 150,
-      toolbar: 
-      [
-        ['style', ['bold', 'italic', 'underline', 'clear']],
-        ['font', ['strikethrough' ]],
-        ['para', ['ul', 'ol'  ]],
-      ]
-  });
 });
 
 function loadComments()
@@ -48,9 +37,9 @@ function loadComments()
                         <div id="comments">
                             <img class="CommentImages" src="../Images/paperclip.png"></img>
                             <img class="CommentImages" src="../Images/delete.png" data-toggle="modal" data-target="#CommentModal" onclick="deletePrompt(${comment.commentId})" role="button"></img>
-                            <img class="CommentImages" src="../Images/edit.png" data-toggle="modal" data-target="#CommentModal" onclick=editPrompt(${comment.commentId}) role="button"></img>
+                            <img class="CommentImages" src="../Images/edit.png" onclick=editComment(${comment.commentId}) role="button"></img>
                             <p>Comment by ${comment.forename + " " + comment.surname}</p>
-                            <p>${comment.commentContent}</p>
+                            <div class="comment${comment.commentId}">${comment.commentContent}</div>
                         </div>
                         `;
                     }).join('')}
@@ -61,14 +50,70 @@ function loadComments()
   xhr.send(data);
 }
 
+function editComment(commentId)
+{
+    $('.comment'+commentId).summernote
+    ({
+      focus: true,
+      enter: false,
+      toolbar: 
+      [
+        ['style', ['bold', 'italic', 'underline', 'clear']],
+        ['font', ['strikethrough' ]],
+        ['para', ['ul', 'ol']],        
+      ]
+    });
+
+    $('.comment'+commentId).on('summernote.keydown', function(we, e) {
+      if(e.shiftKey && e.keyCode == 13)
+      {
+        var newComment = $('.comment'+commentId).summernote('code');
+        var newCommentStripped = newComment.replace(/<[^>]*>?/gm, "");
+        
+        if ($('.comment'+commentId).summernote("isEmpty") || newCommentStripped.trim().length == 0)
+        {
+          console.log("Field is Empty");
+        }		
+        else if (newComment.length > 255)
+        {
+          console.log("Too much characters");
+        }
+        else 
+        {
+          updateComment(commentId, newComment);
+          $('.comment'+commentId).summernote('destroy');       
+        }
+      }
+    });
+}
+
+function updateComment(id, newContent)
+{
+  var data = new FormData();
+  data.append('function', "updateComment");
+  data.append('commentId', id);
+  data.append('newComment', newContent)
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'ticketController.php', true);
+  xhr.onreadystatechange = function() 
+  {
+    if (this.readyState == 4 && this.status == 200)
+      {
+        console.log(this.responseText);
+      }
+  }
+  xhr.send(data);
+}
+
 function saveComment()
 {
   var commentContent = $('.createComment').summernote('code');
-  if ($('.createComment').summernote("isEmpty"))
+  var newCommentStripped = commentContent.replace(/<[^>]*>?/gm, "");
+  if ($('.createComment').summernote("isEmpty") || newCommentStripped.trim().length == 0)
   {
 		document.getElementById("notenoughchars").removeAttribute("hidden");
-		document.getElementById("manychars").setAttribute("hidden");
-		
+		document.getElementById("manychars").setAttribute("hidden");	
   }		
   else if (commentContent.length > 255)
   {
@@ -79,7 +124,6 @@ function saveComment()
   {
     // variable userId is available for User Id logged in
     // variable ticketId is available for Ticket Id
-
     var data = new FormData();
     data.append('function', "createComment");
     data.append('commentContent', commentContent);
@@ -101,20 +145,6 @@ function saveComment()
   }
 }
 
-
-function editPrompt(commentId)
-{
-  document.getElementById("Modal-head").innerHTML = "Edit Comment";
-  document.getElementById("prompt").style.display = "none"
-  document.getElementById("summernoteDiv").style.display = "block"
-	document.getElementById("Modal-footer").innerHTML = 
-	`
-	<div class="modal-footer">
-		<input class="btn btn-primary" type="submit" value="Update Changes" onclick="editComment(${commentId})">
-    </div>
-	`;
-}
-
 function deletePrompt(commentId)
 {
   document.getElementById("Modal-head").innerHTML = "Delete Comment";
@@ -128,14 +158,8 @@ function deletePrompt(commentId)
 	`;
 }
 
-function editComment(commentId)
-{
-  console.log(commentId);
-  $('#CommentModal').modal('hide');
-}
-
 function deleteComment(commentId)
 {
-  console.log(commentId);
-  $('#CommentModal').modal('hide');
+  console.log("Still needs to be implemented.." + "Comment id is " + commentId);
+  $('#CommentModal').modal('hide'); // This should close the modal, make sure this is in the last line of this function
 }
