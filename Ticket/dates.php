528f49601c8ff1a561d772205e0ad8c06247798d
Onik <O_Noor@live.co.uk>
2020-01-14 12:16:29 +0000

    Removed controller and start date and deadline

--- Ticket/dates.php
+++ Ticket/dates.php
@@ -2,14 +2,6 @@
 
 <div ID="dates1">   
 
-<p>Created Date:</p> <label id="createDate"></label> <br>
-<p>Start Date:</p> <label id="startDate"></label> <br>
-
-</div>
-
-<div ID="dates2">
-
-<p>Updated Date:</p> <label id="updateDate"></label><br>
-<p>Deadline:</p> <label id="deadlineDate"></label> <br> 
-
+<p>Created Date:</p> <label id="createDate">Something Like this</label> <br>
+<p>Updated Date:</p> <label id="updateDate">Something like this</label><br>
 </div>
