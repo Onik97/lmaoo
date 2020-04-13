f9b7fb0bbdbf748d2148381c457b088927ad2368
Onik <18009030+OnikNoor97@users.noreply.github.com>
2020-04-13 15:45:52 +0100

    Added Id for automation

--- User/register.php
+++ User/register.php
@@ -14,11 +14,11 @@
 	<div class="container">
   <p id=validateMessage></p>
 	<form action="userController.php" onSubmit="return checkPassword(this)" method='POST'>
-  Forename:<br> <input type="text" name="forename" required> <br>
-  Surname:<br> <input type="text" name="surname" required> <br>
-  Username:<br> <input type="text" name="username" required> <br>
-  Password:<br> <input type="password" name="password1" required> <br>
-  Confirm Password:<br> <input type="password" name="password2" required> <br>
+  Forename:<br> <input type="text" name="forename" id="forenameRegister" required> <br>
+  Surname:<br> <input type="text" name="surname" id="surnameRegister" required> <br>
+  Username:<br> <input type="text" name="username" id="usernameRegister" required> <br>
+  Password:<br> <input type="password" name="password1" id="password1Register" required> <br>
+  Confirm Password:<br> <input type="password" name="password2" id="password2Register" required> <br>
   <input type="hidden" name="function" value="register">
   <input class="one" type="submit" value="Submit"> <br><br>
   <a href="../User/index.php">Register? Login here!</a>
