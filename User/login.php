aa29ec098367c05dc4aebf84ebf9575808e39f99
Tufan Butuner <tufanbutuner@gmail.com>
2020-04-18 17:07:48 +0100

    Navbar changed
    - Navbar fixed to work with Bootstrap 4.4.1

--- User/login.php
+++ User/login.php
@@ -4,12 +4,10 @@ if(isset($_SESSION['userLoggedIn'])){header("Location: ../Ticket/index.php");}
 ?>
 <!DOCTYPE html>
 <html>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="../Css/UserPage.css">
-<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <title>Login</title>
-<head></head>
+<head>
+<?php include("../Global/head.php"); ?>
+</head>
 <?php include("../Global/navBar.php"); ?>
 <body>
 <div class="top-buffer">
