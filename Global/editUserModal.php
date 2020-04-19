d7db158ae693aee91252ab0fd4d5e638fe46cadd
Onik <18009030+OnikNoor97@users.noreply.github.com>
2020-04-19 15:41:05 +0100

    Cleaned up Modal

--- Global/editUserModal.php
+++ Global/editUserModal.php
@@ -1,38 +1,47 @@
 <?php if (!isset($userLoggedIn))  { return; } else { ?>
-            <div class="modal fade" id="view-modal" tabindex="-1" role="dialog" aria-labelledby="view-modal" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                <div class="modal-header">
-                     <h5 class="modal-title" id="exampleModalLongTitle">Account Details</h5>
-                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                    </div>
-                    <form action="../User/userController.php" method="POST">
-                   <div class="modal-body">
+            
+<div class="modal fade" id="view-modal" tabindex="-1" role="dialog" aria-labelledby="view-modal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="EditUserModalTitle">Account Details</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="../User/userController.php" method="POST">
+                <div class="modal-body">
+
                     <div class="form-group">
                         <label>Forename</label>
                         <input class="form-control" value=<?php echo $userLoggedIn->getForename(); ?>  name="editForename" >
                     </div>
+
                     <div class="form-group">
                         <label>Surname</label>
                         <input class="form-control" value=<?php echo $userLoggedIn->getSurname(); ?>  name="editSurname">
                     </div>
+
                     <div class="form-group">
                         <label>Username</label>
                         <input class="form-control" value=<?php echo $userLoggedIn->getUsername(); ?> name="editUsername">
                     </div>
-                    </div>
-                    <input type="hidden" name="function" value="update">
-                    <input type="hidden" name="editUserId" value="<?php echo $userLoggedIn->getId();?>">
-                 <div class="modal-footer">
-                 <input class="btn btn-primary" type="submit" value="Save Changes">
+
                 </div>
-                </form>
-                 </div>
-            </div>
-</div> <?php } ?>
+
+                <input type="hidden" name="function" value="update">
+                <input type="hidden" name="editUserId" value="<?php echo $userLoggedIn->getId();?>">
+
+                    <div class="modal-footer">
+                        <input class="btn btn-primary" type="submit" value="Save Changes">
+                    </div>
+            </form>
         </div>
+    </div>
+</div> 
+
+<?php } ?>
+
 <script>
 		$('form')
 		.each(function(){
