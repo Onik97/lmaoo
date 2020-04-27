e93deebcc7b29589e44a82df75c02b2483e8a7aa
Onik <18009030+OnikNoor97@users.noreply.github.com>
2020-04-27 12:58:41 +0100

    Cleaned up WhiteSpace

--- Script/navBar.js
+++ Script/navBar.js
@@ -1,22 +1,22 @@
 let navBarActive = document.getElementById("navBarActive").innerHTML;
 
 if (navBarActive == "homePage")
-    {
-        document.getElementById("homeNav").classList.add("active")
-    }
-    else if (navBarActive == "aboutPage")
-    {
-        document.getElementById("aboutNav").classList.add("active")
-    }
-    else if (navBarActive == "projectPage")
-    {
-        document.getElementById("projectNav").classList.add("active")
-    }
-    else if (navBarActive == "loginPage")
-    {
-        document.getElementById("accountNav").classList.add("active")
-    }
-    else if (navBarActive == "registerPage" || navBarActive == "userModalPage")
-    {
-        document.getElementById("accountNav").classList.add("active")
-    }
+{
+    document.getElementById("homeNav").classList.add("active")
+}
+else if (navBarActive == "aboutPage")
+{
+    document.getElementById("aboutNav").classList.add("active")
+}
+else if (navBarActive == "projectPage")
+{
+    document.getElementById("projectNav").classList.add("active")
+}
+else if (navBarActive == "loginPage")
+{
+    document.getElementById("accountNav").classList.add("active")
+}
+else if (navBarActive == "registerPage" || navBarActive == "userModalPage")
+{
+    document.getElementById("accountNav").classList.add("active")
+}
