cecc3fa6df1fcd9769593a7121cf75bb705ccaa9
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2020-06-15 16:17:22 +0100

    fixed navbar breaking issue.

--- Script/navBar.js
+++ Script/navBar.js
@@ -1,7 +1,7 @@
 let navBarActive = document.getElementById("navBarActive").innerHTML;
 
-(navBarActive == ("homePage")) ? (document.getElementById("homeNav").classList.add("active"))
-    : (navBarActive == ("aboutPage")) ? (document.getElementById("aboutNav").classList.add("active"))
-    : (navBarActive == ("projectPage") || ("ticketPage")) ? (document.getElementById("projectNav").classList.add("active"))
-    : (navBarActive == ("registerPage") || ("loginPage") || ("adminPage")) ? (document.getElementById("accountNav").classList.add("active"))
-    : console.log("end");
+(navBarActive == "homePage") ? (document.getElementById("homeNav").classList.add("active"))
+    : (navBarActive == "aboutPage") ? (document.getElementById("aboutNav").classList.add("active"))
+    : (navBarActive == "projectPage" || navBarActive == "ticketPage") ? (document.getElementById("projectNav").classList.add("active"))
+    : (navBarActive == "registerPage" || navBarActive == "loginPage" || navBarActive == "adminPage") ? (document.getElementById("accountNav").classList.add("active"))
+    : null;
