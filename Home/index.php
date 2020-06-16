9475d08fd291f92b718b3af259cc98cfca3d92f4
Onik <18009030+OnikNoor97@users.noreply.github.com>
2020-06-16 13:27:08 +0100

    Cleaned up home page

--- Home/index.php
+++ Home/index.php
@@ -1,18 +1,15 @@
-<?php
-require("../User/user.php");
-session_start(); 
-?>
+<?php require("../User/user.php"); session_start(); ?>
+
 <!DOCTYPE html>
 <html>
-<link rel="stylesheet" href="../Css/HomePage.css">
-<title>Home</title>
-<head>
-<?php include("../Global/head.php"); ?>
-</head>
-<p id="navBarActive" hidden>homePage</p>
-<?php include("../Global/navBar.php"); ?>
-<body>
-<?php echo "This page is still under maintenance"; ?>
-<?php include("../Global/editUserModal.php"); ?>
-</body>
+    <link rel="stylesheet" href="../Css/HomePage.css">
+    <title>Home</title>
+    <head>
+        <?php include("../Global/head.php"); ?>  
+    </head>
+
+    <body>
+        <?php include("../Global/navBar.php"); ?>
+        <?php include("../Global/editUserModal.php"); ?>
+    </body>
 </html>
