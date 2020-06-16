88c97711212ddf55aaee3b2f96304f9654b2b0b8
Onik <18009030+OnikNoor97@users.noreply.github.com>
2020-06-16 13:53:29 +0100

    Cleaned up Home Page HTML Code + Added new Message PHP File

--- Home/index.php
+++ Home/index.php
@@ -4,12 +4,15 @@
 <html>
     <link rel="stylesheet" href="../Css/HomePage.css">
     <title>Home</title>
-    <head>
-        <?php include("../Global/head.php"); ?>  
+    <head> 
+        <?php include("../Global/head.php"); ?> 
     </head>
 
     <body>
         <?php include("../Global/navBar.php"); ?>
-        <?php include("../Global/editUserModal.php"); ?>
+        <?php include("../Home/homeMessage.php"); ?>
+        <h1>This page is under maintenance</h1>
     </body>
+        
+        <?php include("../Global/editUserModal.php"); ?>
 </html>
