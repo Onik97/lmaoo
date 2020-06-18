89b67e8cac9fe88d8094f20f5881c08f0d97fac0
Onik <18009030+OnikNoor97@users.noreply.github.com>
2020-06-18 20:59:02 +0100

    Cleaned up Login even more

--- User/login.php
+++ User/login.php
@@ -23,15 +23,16 @@ if(isset($_SESSION['userLoggedIn'])){header("Location: ../Ticket/index.php");}
 			<div class="container">
 				<div class="wrap">
 					<form action="userController.php" method='POST'>
-						<label>Username:</label>
-						<input type="text" name="username" id="usernameLogin" required> <br>
 						
-						<label>Password:</label>
-						<input type="password" name="password" id="passwordLogin" required>
+					<label>Username:</label>
+					<input type="text" name="username" id="usernameLogin" required>
 						
-						<input type="hidden" name="function" value="login">
-						<input class="one" type="submit" value="Submit"> <br><br>
-						<a href="../User/register.php">Not Registered? Click here!</a>
+					<label>Password:</label>
+					<input type="password" name="password" id="passwordLogin" required>
+						
+					<input type="hidden" name="function" value="login">
+					<input class="one" type="submit" value="Submit"> <br><br>
+					<a href="../User/register.php">Not Registered? Click here!</a>
 					</form>
 				</div>	
 			</div>
