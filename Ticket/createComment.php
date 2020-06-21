7a015f68dbdbc580bba7b304dd39490a35a5cd57
Tufan Butuner <tufanbutuner@gmail.com>
2020-06-21 22:14:59 +0100

    Removed empty comment warnings

--- Ticket/createComment.php
+++ Ticket/createComment.php
@@ -1,6 +1,4 @@
 <h1> Create Comment </h1>
-<div ID="notenoughchars" class="alert alert-warning " role="alert" hidden>The text field is empty!</div>
-<div ID="manychars" class="alert alert-warning " role="alert" hidden>Text is to long inside text field!</div>
 <script type="text/javascript" src="../Script/commentController.js"></script>
 <textarea id="createComment" class=createComment name="editordata"></textarea>
 <button id="saveComment" onclick="saveComment('.createComment')">Save Comment</button> 
