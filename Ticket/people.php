71b03703fdd8727da1cbf0fef442b8d537542096
Tufan Butuner <tufanbutuner@gmail.com>
2020-06-22 00:47:35 +0100

    Refactored entire people.php

--- Ticket/people.php
+++ Ticket/people.php
@@ -1,18 +1,12 @@
 <script src="../Script/peopleController.js"></script>
-<p ID="ticketPeople1">Reporter: <label id="reporter"><label></p>
-<p id="reporterUserId" hidden></p>
-<p ID="ticketPeople2">Assignee: <label id="assignee"><label></p>
-<p id="assigneeUserId" hidden></p>
-<button ID="ticketPeopleChooseAssignee" data-toggle="modal" data-target="#CommentModal" onclick="People()">Pick Assignee</button>
+
+<h3>Reporter: </h3>
+    <label id="reporter"></label>
+    <p id="reporterUserId" hidden></p>
+
+<h3>Assignee: </h3>
+    <label id="assignee"></label>
+    <p id="assigneeUserId" hidden></p>
+
+<button id="ticketPeopleChooseAssignee" data-toggle="modal" data-target="#CommentModal" onclick="People()">Pick Assignee</button>
 <button id="ticketPeopleSelfAssignee" onclick="saveAssigneeAsYourself()">Assigned to myself</button>
-
-<div ID="dates1">
-<h1> Dates </h1>
-<p>Created Date:</p> <label id="createDate"></label> <br>
-</div>
-
-<div ID="dates2">
-<p>Updated Date:</p> <label id="updateDate"></label>
-</div>
-
-<script type="text/javascript" src="../Script/dateController.js"></script>
