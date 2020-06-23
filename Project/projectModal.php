e39415edb055460c4daf82d85b2f71dc6e30e8f8
Onik <18009030+OnikNoor97@users.noreply.github.com>
2020-06-23 14:42:59 +0100

    Tidied up Project Modal

--- Project/projectModal.php
+++ Project/projectModal.php
@@ -2,17 +2,17 @@
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
 
-        <div class="modal-header">
-            <h5 class="modal-title modal-title-custom" id="projectModalHead"></h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
+            <div class="modal-header">
+                <h5 class="modal-title modal-title-custom" id="projectModalHead"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
 
-        <div class="modal-body" id="projectModalBody">  
-        </div>
-        <div class="modal-footer" id="projectModalFooter">
-        </div>
+            <div class="modal-body" id="projectModalBody"></div>
+
+            <div class="modal-footer" id="projectModalFooter"></div>
+            
         </div>
     </div>
 </div> 
