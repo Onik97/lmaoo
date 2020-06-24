385b856cc17763a072ae5cc8c83b9379ddb103b2
Tufan Butuner <tufanbutuner@gmail.com>
2020-06-24 19:55:04 +0100

    Dates section added back in

---
+++ Ticket/dates.php
@@ -0,0 +1,11 @@
+<script type="text/javascript" src="../Script/dateController.js"></script>
+
+<div ID="dates1">
+    <h3>Date created:</h3>
+    <label id="createDate"></label>
+</div>
+
+<div ID="dates2">
+    <p>Date updated:</p>
+    <label id="updateDate"></label>
+</div>
