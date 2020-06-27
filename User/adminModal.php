5fcd3d17761cad5de9d464d0b1b7e01c9a539323
Onik <18009030+OnikNoor97@users.noreply.github.com>
2020-06-28 00:29:15 +0100

    Cleaned up HTML + Renamed id for better usage

--- User/adminModal.php
+++ User/adminModal.php
@@ -1,14 +1,14 @@
-<div class="modal fade" id="function-modal" tabindex="-1" role="dialog" aria-labelledby="view-modal" aria-hidden="true">
+<div class="modal fade" id="admin-modal" tabindex="-1" role="dialog" aria-labelledby="view-modal" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
 
             <div class="modal-header">
-                <h5 class="modal-title modal-title-custom" id="modalTitle"></h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
+                <h5 class="modal-title modal-title-custom" id="admin-modal-title"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span></button>
             </div>
 
-            <div class="modal-body"></div>
-            
+            <div class="modal-body" id="admin-modal-body"></div>
+
         </div>
     </div>
 </div>
