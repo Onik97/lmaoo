d831cffe237a4e6dc887c8fd2d10d04f1d83ebd3
Onik <18009030+OnikNoor97@users.noreply.github.com>
2020-06-28 00:27:37 +0100

    Tidied up Modal

--- User/adminModal.php
+++ User/adminModal.php
@@ -7,11 +7,8 @@
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
             </div>
 
-            <div class="modal-body">
-                <div id="modalContent">
-            </div>
-
-            </div>
+            <div class="modal-body"></div>
+            
         </div>
     </div>
 </div>
