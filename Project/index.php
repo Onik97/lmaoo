f189f83dab3a1854add5e56d4dd329764f0be3fd
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2020-07-03 16:46:23 +0100

    Fixed Project page, back to where it was.

--- Project/index.php
+++ Project/index.php
@@ -28,7 +28,6 @@
         <h1 id="ticketMessage">Tickets</h1>
           <div id="ticketDiv">
           
-
             <ul class="nav nav-tabs" id="projectTab" role="tablist">
               <li class="nav-item">
                 <a class="nav-link active" id="comment-tab" data-toggle="tab" href="#comment-content" role="tab" aria-controls="home" aria-selected="true">All</a>
@@ -36,24 +35,20 @@
               <li class="nav-item">
                 <a class="nav-link" id="notes-tab" data-toggle="tab" href="#notes-content" role="tab" aria-controls="profile" aria-selected="false">Assigned to Me</a>
               </li>
-            </ul>
             
             <table id="ticketTable" class="table">
               <div class="tab-pane fade show active" id="nav-all" role="tabpanel" aria-labelledby="nav-home-tab">
-                <ul class="nav nav-tabs nav-fill" id="projectProgressTab">
-                  <li class="nav-item">
-                    <a class="nav-link active" id="open-tab" data-toggle="tab" href="#open-content" role="tab">Open</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" id="in-progress-tab" data-toggle="tab" href="in-progress-content" role="tab">In-progress</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" id="in-automation-tab" data-toggle="tab" href="#in-automation-content" role="tab">In Automation</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" id="completed-tab" data-toggle="tab" href="#completed-content" role="tab">Completed</a>
-                  </li>
                 </ul>
+                    <thead>
+                      <tr>
+                        <th class="col1" scope="col">Ticket ID</th>
+                        <th class="col2" scope="col">Summary</th>
+                        <th class="col3" scope="col">Progress</th>
+                        <th class="col4" scope="col">Assignee</th>
+                      </tr>
+                    </thead>
+                  </table>
+
             <div id="ticketBtnDiv"></div>
           </div>
       </div>
@@ -63,16 +58,3 @@
     <?php include("../Global/editUserModal.php"); ?>
   </body>
 </html>
-
-<!-- 
-<table>
-  <thead>
-    <tr>
-      <th class="col1" scope="col">Ticket ID</th>
-      <th class="col2" scope="col">Summary</th>
-      <th class="col3" scope="col">Progress</th>
-      <th class="col4" scope="col">Assignee</th>
-    </tr>
-</thead>
-</table>  // left this here for when we need it.
--->
