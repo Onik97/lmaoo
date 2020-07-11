7ff35746055a8dd724863ed47bac91523486f5f6
Onik <18009030+OnikNoor97@users.noreply.github.com>
2020-07-11 18:30:35 +0100

    Removed About Page check

--- Script/navBar.js
+++ Script/navBar.js
@@ -1,6 +1,6 @@
 let navBarActive = document.getElementById("navBarActive").innerHTML;
 
 (navBarActive == "homePage") ? (document.getElementById("homeNav").classList.add("active"))
-: (navBarActive == "aboutPage") ? (document.getElementById("aboutNav").classList.add("active"))
+: (navBarActive == "projectPage" || navBarActive == "ticketPage") ? (document.getElementById("projectNav").classList.add("active"))
 : (navBarActive == "registerPage" || navBarActive == "loginPage" || navBarActive == "adminPage") ? (document.getElementById("accountNav").classList.add("active"))
 : null;
