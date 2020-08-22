5bc6a92cbe1eafcfda391950963ee9e7bed7fbda
Onik <18009030+OnikNoor97@users.noreply.github.com>
2020-08-22 20:42:24 +0100

    Allow new db to be used

--- connection.php
+++ connection.php
@@ -4,7 +4,7 @@ function logindb($user, $password)
 	$pdo;
 	try
 	{
-		$pdo = new PDO("mysql:host=192.168.2.115;dbname=lmaoo", $user, $password);
+		$pdo = new PDO("mysql:host=192.168.5.10;dbname=lmaoo", $user, $password);
 		//echo "Connection Successful";
 	}
 	catch(PDOException $e)
