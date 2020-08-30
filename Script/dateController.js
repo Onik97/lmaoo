ba35ea3a9fd62132b35af8b361bc01bb3eb78e0b
Tufan Butuner <tufanbutuner@gmail.com>
2020-08-30 22:58:12 +0100

    Date and Time formatted correctly
    - Code works but could be made more efficient, will try to improve it this week
    
    - AddZero function created because hours and minutes would display "03:07" as "3:7". This code adds a 0 to the beginning of the time if it is less than 10 AM

--- Script/dateController.js
+++ Script/dateController.js
@@ -3,6 +3,11 @@ $(document).ready(function()
   loadDates();
 });
 
+function addZero(i)
+{
+  if (i < 10) { i = "0" + i; } return i; 
+}
+
 function tidyUpTimestamp(timestamp) 
 {
     var dateCreated = new Date(timestamp);
@@ -10,9 +15,14 @@ function tidyUpTimestamp(timestamp)
     var date = dateCreated.getDate();
     var month = dateCreated.getMonth();
     var year = dateCreated.getFullYear();
-    // var time = dateCreated.getTime();
-    var fullDate = `${date}/${month}/${year}`;
+    var hours = dateCreated.getHours();
+    var minutes = addZero(dateCreated.getMinutes());
+    var ampm = (hours >= 12) ? "PM" : "AM";
+    // var seconds = addZero(dateCreated.getSeconds());
+
+    var fullDate = `${date}/${month}/${year} ${hours}:${minutes} ${ampm}`;
     return fullDate;
+    
 }
 
 function loadDates()
@@ -22,7 +32,7 @@ function loadDates()
   loadDatesFromServer(ticketId)
   .then(response => 
   {
-    $("#createDate").html(tidyUpTimestamp(response.data[0].created));
-    $("#updateDate").html(tidyUpTimestamp(response.data[0].updated));
+    $("#createDate").html(tidyUpTimestamp(response.data[0].created.toLocaleString('en-GB')));
+    $("#updateDate").html(tidyUpTimestamp(response.data[0].updated.toLocaleString('en-GB')));
   })
 }
