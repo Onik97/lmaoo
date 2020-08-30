f8ed996bc633224032ef90ed0d4413173a746192
Tufan Butuner <tufanbutuner@gmail.com>
2020-08-31 00:15:06 +0100

    Code cleanup

--- Script/dateController.js
+++ Script/dateController.js
@@ -5,18 +5,16 @@ $(document).ready(function()
 
 function tidyUpTimestamp(timestamp) 
 {
-    var dateCreated = new Date(timestamp);
-    dateCreated.toString();
+    var dateCreated = new Date(timestamp); dateCreated.toString();
     var date = dateCreated.getDate();
     var month = dateCreated.getMonth();
     var year = dateCreated.getFullYear();
     var hours = dateCreated.getHours();
-    var minutes = (dateCreated.getMinutes() < 10) ? ("0" + dateCreated.getMinutes()) : dateCreated.getMinutes(); // to add 0 from 0-9 minutes
-    var ampm = (hours >= 12) ? "PM" : "AM";
-
+    var minutes = dateCreated.getMinutes() < 10 ? "0" + dateCreated.getMinutes() : dateCreated.getMinutes(); // to add 0 from 0-9 minutes
+    var ampm = hours >= 12 ? "PM" : "AM";
+    
     var fullDate = `${date}/${month}/${year} ${hours}:${minutes} ${ampm}`;
     return fullDate;
-    
 }
 
 function loadDates()
