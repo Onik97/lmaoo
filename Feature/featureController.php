d67300a01d733764ff72b750e30ff83114cbea9b
Onik <18009030+OnikNoor97@users.noreply.github.com>
2020-10-10 19:19:11 +0100

    Updated Feature Server Side

--- Feature/featureController.php
+++ Feature/featureController.php
@@ -1,4 +1,19 @@
 <?php 
-// Preparing for future uses
+require_once("../connection.php"); 
+// error_reporting(0);
 
+if($_POST['function'] == "loadFeatures")
+{
+    echo json_encode(loadFeatures($_POST['projectId']));
+}
+else { return; }
+
+function loadFeatures($projectId)
+{
+    $pdo = logindb('user', 'pass');
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    $stmt = $pdo->prepare("SELECT * FROM feature WHERE projectId = ?");
+    $stmt->execute([$projectId]);
+    return $stmt->fetchAll();
+}
 ?>
