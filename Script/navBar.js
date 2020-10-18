80d4d50e01ccc47041fc255397291d51eb02eba9
Onik <18009030+OnikNoor97@users.noreply.github.com>
2020-10-18 13:33:24 +0100

    Project Validation has been implemented

--- Script/navBar.js
+++ Script/navBar.js
@@ -11,14 +11,15 @@ function createProjectPrompt()
 
     let projectNameDiv = $("<div>", {"class" : "form-group modal-content-1"});
     let projectNameLabel = $("<label>").html("Project Name:");
-    let projectNameInput = $("<input>", {class : "form-control", type : "text", id : "projectName", onkeyup : "projectConfirmation()"});
+    let projectNameInput = $("<input>", {class : "form-control", type : "text", id : "projectName", onkeyup : "projectValidation()"});
     $("#globalModalBody").html("").append(projectNameDiv);
     $(projectNameDiv).append(projectNameLabel);
     $(projectNameDiv).append(projectNameInput);
 
     let statusDiv = $("<div>", {"class" : "form-group modal-content-2"});
     let statusLabel = $("<label>").html("Status:");
-    let statusSelect = $("<select>", { id : "projectStatus", "class" : "form-control"}).prop("required", true);
+    let statusSelect = $("<select>", { id : "projectStatus", "class" : "form-control",  onchange : "projectValidation()"}).prop("required", true);
+    let ticketValidationSmall = $("<small>", {id : "projectValidationSmall"});
     $(statusSelect).append($("<option>").val("0").text("").prop({"selected" : true, "disabled" : true}));
     $(statusSelect).append($("<option>").val("Back-log").text("Back-log"));
     $(statusSelect).append($("<option>").val("Development").text("Development"));
@@ -27,15 +28,42 @@ function createProjectPrompt()
     $(statusSelect).append($("<option>").val("Released").text("Released"));
 
     $("#globalModalBody").append(statusDiv);
+    $("#globalModalBody").append(ticketValidationSmall);
     $(statusDiv).append(statusLabel);
     $(statusDiv).append(statusSelect);
 
     $("#globalModalFooter").html("").append($("<button>", {class : "btn btn-primary", type : "text", id : "saveProjectBtn", onclick : "createProject()"}).html("Save"));
+    $('#saveProjectBtn').prop('disabled', true);
 }
 
 function projectValidation() 
 {
+    var data = new FormData();
+    data.append("function", "checkProjectExistance");
+    data.append("name", $.trim($("#projectName").val()));
     
+    if($("#projectStatus").val() == null || $.trim($("#projectName").val()) == "")  { $('#saveProjectBtn').prop('disabled', true); }
+    else 
+    {   
+        axios.post("../Project/projectController.php", data)
+        .then((res) => 
+        {
+            if(res.data)
+            {
+                $("#projectName").addClass("is-invalid");
+                $("#projectValidationSmall").html("Project name not available, must already exist!");
+                $("#projectValidationSmall").addClass("text-danger");
+                $('#saveProjectBtn').prop('disabled', true);
+            }
+            else 
+            {
+                $("#projectName").removeClass("is-invalid");
+                $("#projectValidationSmall").html("");
+                $("#projectValidationSmall").removeClass("text-danger");
+                $('#saveProjectBtn').prop('disabled', false);
+            }
+        })
+    }
 }
 
 function createProject()
