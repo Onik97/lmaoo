a194e287d9c8703b354a4614ba7c7979287ec04c
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2020-10-31 18:46:18 +0000

    chnaged all modal margin/text color to bootstrap

--- Global/editUserModal.php
+++ Global/editUserModal.php
@@ -6,7 +6,7 @@
         <div class="modal-content">
             
             <div class="modal-header">
-                <h5 class="modal-title modal-title-custom" id="EditUserModalTitle">Account Details</h5>
+                <h5 class="modal-title modal-title-custom ml-9 mr-auto text-white" id="EditUserModalTitle">Account Details</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
