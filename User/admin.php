e5899d0d4048dbb416ff4323714ae89e9368aef6
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2020-11-30 20:51:10 +0000

    Added dropbox to the Adminpage

--- User/admin.php
+++ User/admin.php
@@ -13,6 +13,17 @@
         <?php include("../Global/navBar.php"); ?>
         <?php if (!isset($_SESSION['userLoggedIn']) || $userLoggedIn->getLevel() < 3) header("Location: ../Global/forbidden.php"); ?>
 
+    <div class="container d-flex justify-content-center">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+        <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+    </div>
         <div class="container pt-4" id="adminContainer">
             <table class="table table-hover" id="admin-table">
                 <tr>
