9dcb1e6dc92a4d36312193030f440f276c955bfe
Onik <18009030+OnikNoor97@users.noreply.github.com>
2020-12-06 20:10:42 +0000

    Fixed Bug

--- Script/dateController.js
+++ Script/dateController.js
@@ -7,11 +7,11 @@ function tidyUpTimestamp(timestamp)
 {
   var dateCreated = new Date(timestamp); dateCreated.toString();
   var date = dateCreated.getDate();
-  var month = dateCreated.getMonth();
+  var month = dateCreated.getMonth() + 1; // getMonths start with index 0 
   var year = dateCreated.getFullYear();
-  var hours = dateCreated.getHours();
+  var hours = dateCreated.getHours() > 12 ? dateCreated.getHours() - 12 : dateCreated.getHours();
   var minutes = dateCreated.getMinutes() < 10 ? "0" + dateCreated.getMinutes() : dateCreated.getMinutes(); // to add 0 from 0-9 minutes
-  var ampm = hours >= 12 ? "PM" : "AM";
+  var ampm = dateCreated.getHours() >= 12 ? "PM" : "AM";
 
   return `${date}/${month}/${year} ${hours}:${minutes} ${ampm}`;
 }
