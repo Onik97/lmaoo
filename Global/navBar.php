d632e46331d268b886f824d93f87148d229417b4
Onik <18009030+OnikNoor97@users.noreply.github.com>
2020-12-07 13:28:21 +0000

    Using function instead of direct

--- Global/navBar.php
+++ Global/navBar.php
@@ -26,23 +26,7 @@ $userController = new userController();
                     <a id="accountNav" href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Account<span class="caret"></span></a>
 
                     <div class="dropdown-menu">
-                        <?php if (!isset($userLoggedIn)) { ?>
-
-                        <a class="dropdown-item" id="registerNav" href="../User/register.php">Register</a>
-                        <a class="dropdown-item" id="loginNav" href="../User/index.php">Login</a>
-                        
-                        <?php } else if ($userLoggedIn->getLevel() <= 3) { ?>
-
-                        <a class="dropdown-item" id="editAccountNav" data-toggle="modal" data-target="#view-modal" role="button">Edit Account</a> 
-                        <a class="dropdown-item" id="logoutNav" href="../User/logout.php">Logout</a>
-                        
-                        <?php } else { ?>
-                        
-                        <a class="dropdown-item" id="editAccountNav" data-toggle="modal" data-target="#view-modal" role="button">Edit Account</a>   
-                        <a class="dropdown-item" id="logoutNav" href="../User/logout.php">Logout</a>
-                        <a class="dropdown-item" id="adminNav" href="../User/admin.php">Admin</a>
-                        
-                        <?php } ?>
+                        <?php $userController->loadDropdownItems($userLoggedIn); ?>
                     </div>
                 </li>
             </ul>
