92b59798c9b1a01934981905a3edec64a02d743a
Onik <18009030+OnikNoor97@users.noreply.github.com>
2020-12-22 17:26:08 +0000

    Inital Prep for Back end and Front end

---
+++ Manager/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style></style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include("../Global/head.php"); ?> 
+    <link rel="stylesheet" type="text/css" href="../Css/managerPage.css"/>
+    <title>Manager</title>
+</head>
+<body>
+
+    <?php include("../Global/scripts.php"); ?>
+    <?php include("managerModal.php"); ?>
+    <script type="text/javascript" src="../Script/managerController.js"></script>
+</body>
+</html>
