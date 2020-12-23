ae31bfbf4e94b9fee6031d41d397306662069e87
Tufan Butuner <tufanbutuner@gmail.com>
2020-12-23 19:15:57 +0000

    added roles modal

--- Manager/managerModal.php
+++ Manager/managerModal.php
@@ -3,15 +3,31 @@
         <div class="modal-content">
 
             <div class="modal-header">
-                <h5 class="modal-title modal-title-custom ml-9 mr-auto text-white" id="managerModalHead"></h5>
+                <h5 class="modal-title modal-title-custom ml-9 mr-auto text-black" id="managerModalHead">Choose Role</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
             </div>
 
-            <div class="modal-body" id="managerModalBody"></div>
+            <div class="modal-body" id="managerModalBody">
+                <div class="wrapper">
+                    <div class="search-input">
+                        <a href="" target="_blank" hidden></a>
+                        <input type="text" class="form-control" placeholder="User's name">
+                        <div class="autocom-box">
+                            <!-- here list are inserted from javascript -->
+                        </div>
+                        <div class="icon"><i class="fas fa-search"></i></div>
+                    </div>
+                </div>
 
-            <div class="modal-footer" id="managerModalFooter"></div>
+
+            </div>
+
+            <div class="modal-footer" id="managerModalFooter">
+                <button type="button" class="btn btn-success">Save changes</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
             
         </div>
     </div>
