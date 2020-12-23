ed2da9b0216a13f6f3da368a42ca5ec21a362267
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2020-12-23 06:22:49 +0000

    Added Cards with our profile images and details about us

--- Home/homePageDashboard.php
+++ Home/homePageDashboard.php
@@ -7,22 +7,52 @@ if ($userLoggedIn == null)
     <h1 class="display-4">Hard stuck Gold dps ;-;</h1>
   </div>
 </div>
-<div id='devInfo' class='container'>
-    <div class='mx-5'>
+
+<div class='text-center'>
         <h1>List of developer who worked on this Project</h1>
-    </div>
-    <div class='row align-items-start my-auto'>
-        <div class='col-3 my-5'>
-            <p>Section about Onik</p>
+</div>
+
+<div id='devInfo' class='container'>
+    <div id="homePageCards" class="card-deck">
+        <div class="card">
+        <img class="card-img-top" src="../Images/LewisProfile.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Lewis</h5>
+                <p class="card-text">Still hardstuck gold dps, i cri everytime.</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Joined project 1 year ago</small>
+            </div>
         </div>
-        <div class='col-3 my-5'>
-            <p>section about Lewis</p>
+        <div class="card">
+        <img class="card-img-top" src="../Images/AvatarImage.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Adil</h5>
+                <p class="card-text">King Of Heroes</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Joined project 1 year ago</small>
+            </div>
         </div>
-        <div class='col-3 my-5'>
-            <p>[section about Tufan]</p>
+        <div class="card">
+            <img class="card-img-top" src="../Images/AvatarImage.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Onik</h5>
+                <p class="card-text">Also hardstuck gold dps.</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Started project 1 year ago</small>
+            </div>
         </div>
-        <div class='col-3 my-5'>
-            <p>[section about Adil]</p>
+        <div class="card">
+            <img class="card-img-top" src="../Images/AvatarImage.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Tufan</h5>
+                <p class="card-text">UX Genius</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Joined project 1 year ago</small>
+            </div>
         </div>
     </div>
 </div>
