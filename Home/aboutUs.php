4d240d3987558107a83404f42ec4476e42e90f82
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2020-12-23 07:48:12 +0000

    added jumbletron and info cards about the devs inside the aboutUs page

---
+++ Home/aboutUs.php
@@ -0,0 +1,54 @@
+<div class="jumbotron jumbotron-fluid text-center">
+  <div class="container">
+    <h1 class="display-4">Hard stuck Gold dps ;-;</h1>
+  </div>
+</div>
+
+<div class='text-center'>
+        <h1>List of developer who worked on this Project</h1>
+</div>
+
+<div id='devInfo' class='container'>
+    <div id="homePageCards" class="card-deck">
+        <div class="card">
+        <img class="card-img-top" src="../Images/LewisProfile.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Lewis</h5>
+                <p class="card-text">Still hardstuck gold dps, i cri everytime.</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Joined project 1 year ago</small>
+            </div>
+        </div>
+        <div class="card">
+        <img class="card-img-top" src="../Images/AvatarImage.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Adil</h5>
+                <p class="card-text">King Of Heroes</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Joined project 1 year ago</small>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="../Images/AvatarImage.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Onik</h5>
+                <p class="card-text">Also hardstuck gold dps.</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Started project 1 year ago</small>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="../Images/AvatarImage.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Tufan</h5>
+                <p class="card-text">UX Genius</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Joined project 1 year ago</small>
+            </div>
+        </div>
+    </div>
+</div>
