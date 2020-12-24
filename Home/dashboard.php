7d51e30875436bb5410126d99949f52c611eebd1
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2020-12-24 13:43:16 +0000

    adjusted Label text to center

--- Home/dashboard.php
+++ Home/dashboard.php
@@ -3,13 +3,13 @@
     <div id="homePageTickets">
         <div class="homeDashboard">
             <span class="homeProject-list">
-                <p><b>Your projects</b></p>
+                <p class="text-center"><b>Your projects</b></p>
                 <hr class="small-hr">
                 <ul id="homeProjects" class="list-inline">
                 </ul>
             </span>
             <span class="ticket-deadline">
-                <p><b>Ticket Deadlines</b></p>
+                <p class="text-center"><b>Ticket Deadlines</b></p>
                 <hr class="small-hr">
                 <ul id="homeTickets" class="list-inline">
                 </ul>
