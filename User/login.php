4de7c5c17ff30937e99b5cd1b4e7f24bec306b49
Tufan Butuner <tufanbutuner@gmail.com>
2020-12-25 17:06:48 +0000

    Removed previous login html

--- User/login.php
+++ User/login.php
@@ -18,24 +18,7 @@
 											session_unset(); ?>
 		</div><?php
 			} ?>
-	<div class="top-buffer">
-		<div class="container">
-			<div class="wrap">
-				<form action="userController.php" method='POST'>
 
-					<label>Username:</label>
-					<input type="text" name="username" id="usernameLogin" required>
-
-					<label>Password:</label>
-					<input type="password" name="password" id="passwordLogin" required>
-
-					<input type="hidden" name="function" value="login">
-					<input class="one" type="submit" value="Submit"> <br><br>
-					<a href="../User/register.php">Not Registered? Click here!</a>
-				</form>
-			</div>
-		</div>
-	</div>
 	<?php include("../Global/scripts.php"); ?>
 </body>
 
