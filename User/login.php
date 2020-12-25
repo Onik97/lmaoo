44040010a0535639f2ceb84dac03a856853cc736
Tufan Butuner <tufanbutuner@gmail.com>
2020-12-25 17:07:12 +0000

    created new login form

--- User/login.php
+++ User/login.php
@@ -19,6 +19,33 @@
 		</div><?php
 			} ?>
 
+	<main class="d-flex align-items-center min-vh-100 py-3 py-md-0">
+    <div class="container">
+    	<div class="card">
+    		<div class="row no-gutters">
+    			<div class="col-md-6">
+					<img id="otal_logo" src="../Images/otal_logo.png">
+				</div>
+			<div class="col-md-6 text-center">
+            	<div class="card-body">
+					<h2 class="header">Member login</h2>
+					<form action="userController.php" method='POST'>
+						<div class="form-group">
+							<input type="text" class="form-control" name="username" placeholder="Username" id="usernameLogin" required>
+						</div>
+						<div class="form-group mb-4">
+							<input type="password" class="form-control" name="password" placeholder="Password" id="passwordLogin" required>
+						</div>
+						<input type="hidden" name="function" value="login">
+						<button type="submit" value="Submit" class="btn btn-success btn-block">Login</button>
+						<div class="form-group mt-3">
+							<a class="register" href="../User/register.php">Not Registered? Click here!</a>
+						</div>
+            		</form>
+            	</div>
+          	</div>
+        </div>
+    </div>
 	<?php include("../Global/scripts.php"); ?>
 </body>
 
