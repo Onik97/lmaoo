38b9ae26763b9e982875cd4d6223b1576cc12ead
Tufan Butuner <tufanbutuner@gmail.com>
2020-12-25 23:15:00 +0000

    added font awesome for icons

--- Global/head.php
+++ Global/head.php
@@ -4,6 +4,8 @@
 <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.css" rel="stylesheet">
 <link rel="stylesheet" href="../Css/notifications.css">
 
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
 <script>
 <?php 
 if(isset($_SESSION['userLoggedIn'])) 
