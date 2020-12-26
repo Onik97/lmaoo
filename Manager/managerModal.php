c8e71aa17dbf23680d982466d42d2f7fe7dbe34c
Onik <18009030+OnikNoor97@users.noreply.github.com>
2020-12-26 14:30:23 +0000

    Removed Static Data

--- Manager/managerModal.php
+++ Manager/managerModal.php
@@ -8,59 +8,18 @@
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-
-
             <!-- Search bar to look for members on the platform -->
-
             <div class="modal-body" id="managerModalBody">
                 <div class="wrapper">
                     <input type="text" class="search-input form-control" placeholder="User's name">
-                    <div class="autocom-box">
-                        <!-- here list are inserted from javascript -->
-                        <li>Static</li>
-                        <li>Data</li>
-                        <li>for</li>
-                        <li>Tufan</li>
-                    </div>
+                    <div class="autocom-box"><!-- here list are inserted from javascript --></div>
                 </div>
 
                 <!-- List of members on the platform  -->
-
-                <ul class="list-group list-group-flush user-list">
-                    <li class="list-group-item users">
-                        <div class="user-info">
-                            <span id="tufan">Static Data for Tufan 1</span>
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown">ACTIVE ROLE?</button>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item" value="1">Manager</a>
-                                    <a class="dropdown-item" value="0">Developer</a>
-                                </div>
-                            </div>
-                        </div>
-                        <i class="fas fa-times"></i>
-                    </li>
-
-                    <li class="list-group-item users">
-                        <div class="user-info">
-                            <span id="tufan">Static Data for Tufan 2</span>
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown">ACTIVE ROLE?</button>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item" value="1">Manager</a>
-                                    <a class="dropdown-item" value="0">Developer</a>
-                                </div>
-                            </div>
-                        </div>
-                        <i class="fas fa-times"></i>
-                    </li>
-                </ul>
+                <ul class="list-group list-group-flush user-list"></ul>
             </div>
 
-            <div class="modal-footer" id="managerModalFooter">
-                <button type="button" class="btn btn-success">Save changes</button>
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            </div>
+            <div class="modal-footer" id="managerModalFooter"></div>
 
         </div>
     </div>
