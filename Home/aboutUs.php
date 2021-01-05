7a09671eb1953030b113753e33b901a0fc1ade50
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-01-05 23:22:03 +0000

    Updated About Us Page

--- Home/aboutUs.php
+++ Home/aboutUs.php
@@ -10,45 +10,48 @@
 
 <div id='devInfo' class='container'>
     <div id="homePageCards" class="card-deck">
-        <div class="card">
-        <img class="card-img-top" src="../Images/LewisWelch.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Lewis</h5>
-                <p class="card-text">Still hardstuck gold dps, i cri everytime.</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">Joined project 1 year ago</small>
-            </div>
+    <div class="card">
+        <img class="card-img-top" src="../Images/OnikNoor.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Onik</h5>
+            <p class="card-text">Project Lead & Back-end Lead</p>
         </div>
-        <div class="card">
+        <div class="card-footer">
+            <small class="text-muted">Started project 1 year ago</small>
+        </div>
+    </div>
+
+    <div class="card">
         <img class="card-img-top" src="../Images/profilePictures/avatar.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Adil</h5>
-                <p class="card-text">King Of Heroes</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">Joined project 1 year ago</small>
-            </div>
+        <div class="card-body">
+            <h5 class="card-title">Tufan</h5>
+            <p class="card-text">UX Lead</p>
         </div>
-        <div class="card">
-            <img class="card-img-top" src="../Images/profilePictures/avatar.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Onik</h5>
-                <p class="card-text">Also hardstuck gold dps.</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">Started project 1 year ago</small>
-            </div>
+        <div class="card-footer">
+            <small class="text-muted">Joined project 1 year ago</small>
         </div>
-        <div class="card">
-            <img class="card-img-top" src="../Images/profilePictures/avatar.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Tufan</h5>
-                <p class="card-text">UX Genius</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">Joined project 1 year ago</small>
-            </div>
+    </div>
+
+    <div class="card">
+    <img class="card-img-top" src="../Images/profilePictures/avatar.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Adil</h5>
+            <p class="card-text">Software Developer</p>
         </div>
+        <div class="card-footer">
+            <small class="text-muted">Joined project 1 year ago</small>
+        </div>
+    </div>
+
+    <div class="card">
+    <img class="card-img-top" src="../Images/LewisWelch.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Lewis</h5>
+            <p class="card-text">Front-end Lead</p>
+        </div>
+        <div class="card-footer">
+            <small class="text-muted">Joined project 1 year ago</small>
+        </div>
+    </div>
     </div>
 </div>
