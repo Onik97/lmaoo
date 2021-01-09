3775eb6e365f1bccfd0f14a7a65c435726466d22
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-01-09 00:06:52 +0000

    #331 Get And Set Access Token Implemented

--- Github/githubController.php
+++ Github/githubController.php
@@ -3,4 +3,29 @@
 class githubController
 {
     public function __construct() { $this->config = include('../config.php'); } 
+
+    public function setAccessToken()
+    {
+        $postRequest = array(
+            'client_id' => $this->config['github_clientId'],
+            'client_secret' => $this->config['github_secret'],
+            'code' => $_GET['code'],
+        );
+        
+        $cURLConnection = curl_init('https://github.com/login/oauth/access_token');
+        curl_setopt($cURLConnection, CURLOPT_POSTFIELDS, $postRequest);
+        curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($cURLConnection, CURLOPT_HTTPHEADER, array('Accept: application/json'));
+        $accessTokenResponse = curl_exec($cURLConnection);
+        curl_close($cURLConnection);
+
+        $jsonAccessTokenResponse = json_decode($accessTokenResponse, true);
+        $this->accessToken = $jsonAccessTokenResponse["access_token"];
+    }
+
+    public function getAccessToken()
+    {
+        return $this->accessToken;
+    }
+
 }
