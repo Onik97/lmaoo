25e57f4328fcaaf7b4a4f7dc267ad1a88785566b
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-01-09 00:06:11 +0000

    #331 GithubController + constructor

---
+++ Github/githubController.php
@@ -0,0 +1,6 @@
+<?php require_once("../connection.php"); require_once("../User/userController.php");
+
+class githubController
+{
+    public function __construct() { $this->config = include('../config.php'); } 
+}
