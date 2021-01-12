6cfe4b2c8198ded2aa95dc2486805907f9e38170
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-01-12 16:18:13 +0000

    GET and POST added in Wrapper

--- Utility/ApiWrapper.php
+++ Utility/ApiWrapper.php
@@ -2,4 +2,28 @@
 
 class ApiWrapper 
 {
+    public static function getRequest(string $url, array $headers = null)
+    {
+        $cURLConnection = curl_init();
+        curl_setopt($cURLConnection, CURLOPT_URL, $url);
+        curl_setopt($cURLConnection, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+        if ($headers != null) curl_setopt($cURLConnection, CURLOPT_HTTPHEADER, $headers);
+
+        $response = curl_exec($cURLConnection);
+        curl_close($cURLConnection);
+        return $response;
+    }
+
+    public static function postRequest(string $url, array $postFields = null, array $headers = null)
+    {
+        $cURLConnection = curl_init($url);
+        if($postFields != null) curl_setopt($cURLConnection, CURLOPT_POSTFIELDS, $postFields);
+        if($headers != null) curl_setopt($cURLConnection, CURLOPT_HTTPHEADER, $headers);
+        
+        curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($cURLConnection);
+        curl_close($cURLConnection);
+        return $response;
+    }
 }
