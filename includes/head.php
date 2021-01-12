ec9e2eaa45ac0ef1c9af2305334d92c4fcfd8642
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-01-12 14:39:28 +0000

    head.php clean up

--- includes/head.php
+++ includes/head.php
@@ -1,3 +1,4 @@
+<?php require_once($_SERVER["DOCUMENT_ROOT"] . "lmaoo/includes/autoloader.inc.php"); ?>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -5,9 +6,8 @@
 <link rel="stylesheet" href="../Css/notifications.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 
-<script> <?php require_once($_SERVER["DOCUMENT_ROOT"] . "lmaoo/includes/autoloader.inc.php"); 
+<script> <?php
 $userController = new userController();
-
 if(isset($_SESSION['userLoggedIn'])) 
 {
   $userLoggedIn = unserialize($_SESSION['userLoggedIn']);
