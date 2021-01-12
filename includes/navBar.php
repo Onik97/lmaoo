fe392b11d67a54767058bcf238aef39b175f5cbe
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-01-12 14:08:44 +0000

    Cleaned up NavBar with Autoloader

--- includes/navBar.php
+++ includes/navBar.php
@@ -1,12 +1,10 @@
-<?php ob_start(); require_once("../Project/projectController.php"); require_once("../Ticket/ticketController.php"); require_once("../User/userController.php");
-$projectController = new projectController();
-$ticketController = new ticketController();
-$userController = new userController();
+<?php ob_start(); require_once($_SERVER["DOCUMENT_ROOT"] . "lmaoo/includes/autoloader.inc.php");
+if(!isset($_SESSION['userLoggedIn'])) $_SESSION['userLoggedIn'] = null;
 ?>
 <link rel="stylesheet" href="../Css/navbar.css">
 <nav class="navbar navbar-expand-lg">
 
-    <?php $userController->loadDarkModeToggle($_COOKIE["lmaooDarkMode"], $userLoggedIn); ?>
+    <?php UserController::loadDarkModeToggle($_COOKIE["lmaooDarkMode"], $_SESSION['userLoggedIn']); ?>
 
     <div class="container">
 
@@ -15,18 +13,18 @@ $userController = new userController();
         </button>
 
         <div class="collapse navbar-collapse w-100 order-3 dual-collapse2" id="navbarNav">
-            <?php $ticketController->loadSearchBar($userLoggedIn) ?>
+            <?php TicketController::loadSearchBar($_SESSION['userLoggedIn']); ?>
 
             <ul class="nav navbar-nav ml-auto mr-5">
 
                 <li class="nav-item"> <a class="nav-link" id="homeNav" href="../Home/index.php">Home</a> </li>
-                <?php $projectController->loadProjectsInNavBar($userLoggedIn) ?>
+                <?php ProjectController::loadProjectsInNavBar($_SESSION['userLoggedIn']); ?>
 
                 <li class="nav-item dropdown">
                     <a id="accountNav" href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Account<span class="caret"></span></a>
 
                     <div class="dropdown-menu">
-                        <?php $userController->loadDropdownItems($userLoggedIn); ?>
+                        <?php UserController::loadDropdownItems($_SESSION['userLoggedIn']); ?>
                     </div>
                 </li>
             </ul>
