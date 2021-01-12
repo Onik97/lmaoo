ee5a557fe0c5a0420f338e4c51a99e05b245b58d
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-01-12 15:20:17 +0000

    Better error reporting and cleaner use of session_start

--- Utility/Library.php
+++ Utility/Library.php
@@ -4,7 +4,7 @@ class Library
 {
     public static function logindb()
 	{
-		$config = include(__DIR__ . '/../config.php');
+		$config = include($_SERVER["DOCUMENT_ROOT"] . 'lmaoo/config.php');
 		try
 		{
 			$pdo = new PDO("mysql:host={$config['db_host']};dbname={$config['db_table']}", $config['db_username'],  $config['db_password']);
--- includes/autoloader.inc.php
+++ includes/autoloader.inc.php
@@ -2,10 +2,12 @@
 
 function autoLoader($className)
 {
+    if (!file_exists(($_SERVER["DOCUMENT_ROOT"] . "lmaoo/config.php"))) die("ERROR:101");
+    
     $paths = array($_SERVER["DOCUMENT_ROOT"] . "lmaoo/Controller/$className.php",
                    $_SERVER["DOCUMENT_ROOT"] . "lmaoo/Utility/$className.php",
                    $_SERVER["DOCUMENT_ROOT"] . "lmaoo/Model/$className.php"); 
 
-    foreach ($paths as $path) if (file_exists($path)) include_once($path); 
-    session_start();
+    foreach ($paths as $path) if (file_exists($path)) include_once($path);
+    if (session_status() == PHP_SESSION_NONE) session_start();
 }
