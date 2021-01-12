7dfa4f213ba4947e0c9109568adecdf0917fbe83
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-01-12 15:42:02 +0000

    Moved Feature controller to controller folder

--- Controller/FeatureController.php
+++ Controller/FeatureController.php
@@ -1,25 +1,10 @@
-<?php require_once(__DIR__ . "/../connection.php");
-$featureController = new featureController();
+<?php require_once($_SERVER["DOCUMENT_ROOT"] . "lmaoo/includes/autoloader.inc.php"); 
 
-if($_POST['function'] == "loadFeatures")
-{
-    echo json_encode($featureController->loadFeatures($_POST['projectId']));
-}
-else if ($_POST['function'] == "checkFeatureExistance")
-{
-    echo $featureController->featureExistance($_POST['featureName'], $_POST['projectId']);
-}
-else if ($_POST['function'] == "createFeature")
-{
-    $featureController->createFeature($_POST['featureName'], $_POST['projectId']);
-}
-else { return; }
-
-class featureController
+class FeatureController
 {
     public function loadFeatures($projectId)
     {
-        $pdo = logindb();
+        $pdo = Library::logindb();
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
         $stmt = $pdo->prepare("SELECT * FROM feature WHERE projectId = ?");
         $stmt->execute([$projectId]);
@@ -28,7 +13,7 @@ class featureController
 
     public function featureExistance($featureName, $projectId)
     {
-        $pdo = logindb();
+        $pdo = Library::logindb();
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
         $stmt = $pdo->prepare("SELECT name FROM feature WHERE name = ? AND projectId = ?");
         $stmt->execute([$featureName, $projectId]);
@@ -38,7 +23,7 @@ class featureController
 
     public function createFeature($featureName, $projectId)
     {
-        $pdo = logindb();
+        $pdo = Library::logindb();
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
         $stmt = $pdo->prepare("INSERT INTO feature (name, projectId) VALUES (?, ?)");
         $stmt->execute([$featureName, $projectId]);
