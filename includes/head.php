9100787ef295b5b1299488ad622c9f86d5b6d883
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-01-12 14:08:02 +0000

    Head Clean up with Autoloader

--- includes/head.php
+++ includes/head.php
@@ -3,15 +3,14 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.css" rel="stylesheet">
 <link rel="stylesheet" href="../Css/notifications.css">
-
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 
-<script>
-<?php 
+<script> <?php require_once($_SERVER["DOCUMENT_ROOT"] . "lmaoo/includes/autoloader.inc.php"); 
+$userController = new userController();
+
 if(isset($_SESSION['userLoggedIn'])) 
 {
-  $userLoggedIn = $_SESSION['userLoggedIn'];
-  include_once("../User/user.php");
+  $userLoggedIn = unserialize($_SESSION['userLoggedIn']);
   
   echo "const userId = '" . $userLoggedIn->getId() . "'\n";
   echo "const userForename = '" . $userLoggedIn->getForename(). "'\n";
@@ -21,10 +20,7 @@ if(isset($_SESSION['userLoggedIn']))
 
   if ($userLoggedIn->getLevel() > 1)
   {
-    include_once("../User/userController.php");
     echo "const users = " . json_encode($userController->getActiveUsers()) . "\n";
   }
 } 
-?>
-
-</script>
+?> </script>
