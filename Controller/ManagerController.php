31f902962c55fa12b4d4e470e06853d0e83c65b4
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-01-12 15:49:34 +0000

    Moved to Target

--- Controller/ManagerController.php
+++ Controller/ManagerController.php
@@ -1,32 +1,4 @@
-<?php include("../../includes/autoloader.inc.php");
-
-$managerController = new ManagerController();
-
-if ($function == "loadOwnerProjects") 
-{
-    Validator::validateManager();
-    echo json_encode($managerController->loadOwnerProjects());
-}
-else if ($function == "loadManagerProjects")
-{
-    Validator::validateManager();
-    echo json_encode($managerController->loadManagerProjects());
-}
-else if ($function == "removeUsersFromProject")
-{
-    Validator::validateManager();
-    echo json_encode($managerController->removeUsersFromProject($_POST['projectId']));
-}
-else if ($function == "addUsersToProject")
-{
-    Validator::validateManager();
-    echo $managerController->addUsersToProject($_POST['json']);
-}
-else if ($function == "loadUsersOnProject")
-{
-    Validator::validateManager();
-    echo json_encode($managerController->loadUsersOnProject($_POST['projectId']));
-}
+<?php require_once($_SERVER["DOCUMENT_ROOT"] . "lmaoo/includes/autoloader.inc.php"); 
 
 class ManagerController
 {
