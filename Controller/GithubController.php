b994c3892900f00d6fa54b49afe2b7d4f037950a
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-01-14 14:55:14 +0000

    Create Static method for loading profile

--- Controller/GithubController.php
+++ Controller/GithubController.php
@@ -72,4 +72,24 @@ class GithubController extends ApiWrapper
         $_SESSION['message'] = 'Github Registration Successful!';
         header("Location: ../src/Home/index.php");
     }
+
+    public static function loadProfile($userLoggedIn)
+    {
+        if($userLoggedIn->getGithubId() == null)
+        {
+            echo 
+            "<div class='form-group'>
+            <a href='../Github/authorize.php?function=register' class='github'><i class='fab fa-github'></i> Register on Github</a>
+            </div>";
+        }
+        else
+        {
+            echo 
+            "<div class='row github-registered'>
+            <div class='col-2'><img class='github-image' width='50' height='50' src='{$userLoggedIn->profilePicture}}'></div>
+            <div class='col-10 github-info'><i class='fab fa-github'></i> Github Linked as {$userLoggedIn->name} ({$userLoggedIn->login})</div>   
+            </div>";
+        }
+    }
+
 }
