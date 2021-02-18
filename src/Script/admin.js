59299edc15c1480325fa0bfdbf2eaabc6e9d1f61
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2021-02-18 18:02:39 +0000

    removed unused code

--- src/Script/admin.js
+++ src/Script/admin.js
@@ -236,11 +236,5 @@ function editUserPrompt(userIdSelected)
 
     $('#editUserSelecter').on('change',(event) => {
 
-
-        (event.target.value == 1) ? $('#admin-modal-footer').children().remove()
-        : (event.target.value == 2) ? $("#admin-modal-footer").html("").append($("<button>", {class : "btn btn-danger", type : "text", id : "updateUserBtn1", onclick : `updateUserLevelPrompt(${userIdSelected})`}).html("Update User"))
-        : (event.target.value == 3) ? $("#admin-modal-footer").html("").append($("<button>", {class : "btn btn-danger", type : "text", id : "updateUserBtn2", onclick : `editPasswordPrompt(${userIdSelected})`}).html("Edit Password (wip)"))
-        : (event.target.value == 4) ? $("#admin-modal-footer").html("").append($("<button>", {class : "btn btn-danger", type : "text", id : "updateUserBtn3", onclick : `passwordResetPrompt(${userIdSelected})`}).html("Reset Password"))
-        : null;
     });
 }
