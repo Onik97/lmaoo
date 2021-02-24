e798a7a4ad8fc8ad88fef1ef9bf9411a1e5c4a91
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-02-24 22:29:00 +0000

    DataTypeUtility Class Implemented

---
+++ src/Script/Utility/DataTypeUtility.js
@@ -0,0 +1,14 @@
+export default class DataTypeUtility {
+    
+    static SerializedArrayToJSON(serializeArray)
+    {
+        var indexed_array = {};
+
+        serializeArray.map(content => {
+            indexed_array[content["name"]] = content["value"]
+        })
+
+        return indexed_array;
+    }
+    
+}
