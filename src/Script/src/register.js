d962f89f2fdd7359604e236e96bcd28a5236e720
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-02-24 22:31:16 +0000

    Register.js - Passwords now validated

---
+++ src/Script/src/register.js
@@ -0,0 +1,20 @@
+import RegisterController from "../Controller/RegisterController.js";
+import DataTypeUtility from "../Utility/DataTypeUtility.js";
+import Validator from "../Utility/Validator.js";
+
+//Submit Register Form
+$("#registerForm").submit(function() {
+
+    let registerData = DataTypeUtility.SerializedArrayToJSON($(this).serializeArray());
+    let error = [];
+
+    if (registerData.password1 != registerData.password2) error.push("Password does not match!");
+    if (!Validator.validateMinimumLength(registerData.password1, 5)) error.push("Password must be at least 6 characters long");
+    if (!Validator.validateNumber(registerData.password1)) error.push("Password must have a number");
+    if (!Validator.validateSpecialCharacter(registerData.password1)) error.push("Password must have a special character");
+    
+     $("#validateMessage").html(""); // clears to avoid duplicates
+     error.forEach((e) => document.getElementById("validateMessage").innerHTML += `${e}<br>`);
+     if(error.length != 0) document.getElementById("validateMessage").removeAttribute("hidden");
+     return error.length == 0 ? true : false;
+});
