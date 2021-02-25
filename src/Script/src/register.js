5f3cf7bf3870755cb8324041abafbf0cce8a4387
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-02-25 19:36:24 +0000

    Cleaned up Register Validation

--- src/Script/src/register.js
+++ src/Script/src/register.js
@@ -13,8 +13,9 @@ $("#registerForm").submit(function() {
     if (!Validator.validateNumber(registerData.password1)) error.push("Password must have a number");
     if (!zValidator.validateSpecialCharacter(registerData.password1)) error.push("Password must have a special character");
     
-     $("#validateMessage").html(""); // clears to avoid duplicates
-     error.forEach((e) => document.getElementById("validateMessage").innerHTML += `${e}<br>`);
-     if(error.length != 0) document.getElementById("validateMessage").removeAttribute("hidden");
-     return error.length == 0 ? true : false;
+    $("#validateMessage").html(""); // clears to avoid duplicates
+    error.forEach((e) => document.getElementById("validateMessage").innerHTML += `${e}<br>`);
+    if(error.length != 0) document.getElementById("validateMessage").removeAttribute("hidden");
+    return error.length == 0 && $("#usernameMessage").html() == "" ? true : false;
 });
+
