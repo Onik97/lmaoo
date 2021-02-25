76404bd3e16b1497fab7cf58b750fa5b94bb7538
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-02-25 19:38:02 +0000

    Cleaned up HTML + removed comments + button disabled for defensive programming

--- src/User/register.php
+++ src/User/register.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<html lang="en">
 <title>Register Page</title>
 
 <head>
@@ -14,45 +15,54 @@
     <div class="container">
       <div class="card">
         <div class="row no-gutters">
+
           <div class="col-md-6">
             <img id="otal_logo" src="../Images/otal_light.png">
           </div>
+
           <div class="col-md-6 text-center">
             <div class="card-body">
               <h2 class="register-header">Sign up</h2>
-              <!-- onSubmit="return registerValidation()" onkeyup="validateUsername()" -->
-              <form id="registerForm" name="registerForm" action="target.php"  method='POST'>
+              <form id="registerForm" name="registerForm" action="target.php" method='POST'>
+
                 <div class="form-group">
                   <input type="text" class="form-control" name="forename" placeholder="First name" id="forenameRegister" required>
                 </div>
+
                 <div class="form-group">
                   <input type="text" class="form-control" name="surname" placeholder="Last name" id="surnameRegister" required>
                 </div>
+
                 <div class="form-group">
                   <input type="text" class="form-control" name="username" placeholder="Username" id="usernameRegister" required>
                   <small id="usernameMessage"></small>
                 </div>
+
                 <div class="form-group">
                   <input type="password" class="form-control" name="password1" placeholder="Password" id="password1Register" required>
                 </div>
+
                 <div class="form-group">
                   <input type="password" class="form-control" name="password2" placeholder="Confirm password" id="password2Register" required>
                 </div>
+
                 <input type="hidden" name="function" value="register">
-                <button id="registerBtn" type="submit" value="Submit" class="btn btn-success btn-block">Register</button>
+                  <button id="registerBtn" type="submit" value="Submit" class="btn btn-success btn-block" disabled>Register</button>
                 <small id="validateMessage" hidden></small>
+
                 <div class="form-group mt-3">
                   <a class="register" href="../User/index.php">Already Registered? Login here!</a>
                 </div>
+
               </form>
             </div>
           </div>
         </div>
       </div>
+    </div>
+  </main>
 
-      <?php include("../../includes/scripts.php"); ?>
-      <script type="module" src="../Script/src/register.js"></script>
-      <script src="../Script/register.js"></script>
+  <?php include("../../includes/scripts.php"); ?>
+  <script src="../Script/register.js"></script>
 </body>
-
 </html>
