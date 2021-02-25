17353d8ec166bf3ecaa3e06e0d2b733e04cff55e
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-02-25 19:37:17 +0000

    Corrected URL + Removed unnecessary imports

--- src/Script/Controller/RegisterController.js
+++ src/Script/Controller/RegisterController.js
@@ -1,10 +1,8 @@
 import AxiosWrapper from "../Utility/AxiosWrapper.js"
-import DataTypeUtility from "../Utility/DataTypeUtility.js"
-import Validator from "../Utility/Validator.js"
 
 export default class RegisterController {
     
     static async validateUsername(username) {
-        return await AxiosWrapper.QuickPostRequest("../../User/target.php", "checkUsername", "username", username);
+        return await AxiosWrapper.QuickPostRequest("../User/target.php", "checkUsername", "username", username);
     }
 }
