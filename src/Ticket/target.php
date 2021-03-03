f831b981def49435de11033eaa4b9c2d20527a54
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-03-03 22:32:35 +0000

    Convert all static + Using Error Control Operator

--- src/Ticket/target.php
+++ src/Ticket/target.php
@@ -1,89 +1,26 @@
 <?php include_once(__DIR__ . "/../../includes/autoloader.inc.php");
 
-$ticketController = new TicketController();
-$function = $_POST['function'];
-
-if ($function == "checkTicket")
+try
 {
-    Validator::validateDeveloper();
-    echo $ticketController->ticketIdExistance($_POST['ticketId']);
+    if (!Validator::validateUserLoggedIn()) { http_response_code(401); return; }
+    RouteController::Post("checkTicket", Validator::validateDeveloper(), 'TicketController::ticketIdExistance', [@$_POST["ticketId"]]);
+    RouteController::Post("checkTicketExistance", Validator::validateDeveloper(), 'TicketController::ticketExistance', [@$_POST['ticketName'], @$_POST['featureId']]);
+    RouteController::Post("createComment", Validator::validateDeveloper(), 'TicketController::createComment', [@$_POST['commentContent'], @$_POST['ticketId'], @$_POST['userId']]);
+    RouteController::Post("loadComments", Validator::validateDeveloper(), 'TicketController::loadComments', [@$_POST["ticketId"]]);
+    RouteController::Post("updateComment", Validator::validateDeveloper(), 'TicketController::updateComment', [@$_POST['commentId'], @$_POST['commentContent']]);
+    RouteController::Post("deleteComment", Validator::validateDeveloper(), 'TicketController::deleteComment', [@$_POST["commentId"]]);
+    RouteController::Post("saveSelectedAssignee", Validator::validateDeveloper(), 'TicketController::saveSelectedAssignee', [@$_POST["ticketId"], @$_POST['assigneeId']]);
+    RouteController::Post("assigneeSelf", Validator::validateDeveloper(), 'TicketController::assigneeYourself', [@$_POST["ticketId"], @$_POST['selfId']]);
+    RouteController::Post("loadAssignee", Validator::validateDeveloper(), 'TicketController::loadAssignee', [@$_POST["ticketId"]]);
+    RouteController::Post("loadReporter", Validator::validateDeveloper(), 'TicketController::loadReporter', [@$_POST["ticketId"]]);
+    RouteController::Post("updateTicketTime", Validator::validateDeveloper(), 'TicketController::updateTicketTime', [@$_POST["ticketId"]]);
+    RouteController::Post("loadSummary", Validator::validateDeveloper(), 'TicketController::loadSummary', [@$_POST["ticketId"]]);
+    RouteController::Post("loadProgress", Validator::validateDeveloper(), 'TicketController::loadProgress', [@$_POST["ticketId"]]);
+    RouteController::Post("changeProgress", Validator::validateDeveloper(), 'TicketController::changeProgress', [@$_POST['progress'], @$_POST["ticketId"]]);
+    RouteController::Post("saveSummary", Validator::validateDeveloper(), 'TicketController::saveSummary', [@$_POST['summary'], @$_POST["ticketId"]]);
+    Validator::ThrowNotFound();
 }
-else if ($function == "checkTicketExistance")
+catch(Throwable $e)
 {
-    Validator::validateDeveloper();
-    echo $ticketController->ticketExistance($_POST['ticketName'], $_POST['featureId']);
-}
-else if ($function == "createComment")
-{
-    Validator::validateDeveloper();
-    $ticketController->createComment($_POST['commentContent'], $_POST['ticketId'], $_POST['userId']);
-}
-else if ($function == "loadComments")
-{
-    Validator::validateDeveloper();
-    echo json_encode($ticketController->loadComments($_POST['ticketId']));
-}
-else if ($function == "updateComment")
-{
-    Validator::validateDeveloper();
-    echo $ticketController->updateComment($_POST['commentId'], $_POST['commentContent']);
-}
-else if ($function == "deleteComment")
-{
-    Validator::validateDeveloper();
-    echo $ticketController->deleteComment($_POST['commentId']);
-}
-else if ($function == "saveSelectedAssignee")
-{
-    Validator::validateDeveloper();
-    echo $ticketController->saveSelectedAssignee($_POST['ticketId'], $_POST['assigneeId']);
-}
-else if ($function == "assigneeSelf")
-{
-    Validator::validateDeveloper();
-    echo $ticketController->assigneeYourself($_POST['ticketId'], $_POST['selfId']);
-}
-else if ($function == "loadDates")
-{
-    Validator::validateDeveloper();
-    echo json_encode($ticketController->loadDates($_POST['ticketId']));
-}
-else if ($function == "loadAssignee")
-{
-    Validator::validateDeveloper();
-    echo json_encode($ticketController->loadAssignee($_POST["ticketId"]));
-}
-else if ($function == "loadReporter")
-{
-    Validator::validateDeveloper();
-    echo json_encode($ticketController->loadReporter($_POST['ticketId']));
-}
-else if ($function == "updateTicketTime")
-{
-    Validator::validateDeveloper();
-    $ticketController->updateTicketTime($_POST["ticketId"]);
-}
-else if($function == "loadSummary")
-{
-    Validator::validateDeveloper();
-    echo $ticketController->loadSummary($_POST['ticketId']);
-}
-else if ($function == "loadProgress")
-{
-    Validator::validateDeveloper();
-    echo $ticketController->loadProgress($_POST['ticketId']);
-}
-else if ($function == "changeProgress")
-{
-    Validator::validateDeveloper();
-    echo $ticketController->changeProgress($_POST['progress'], $_POST['ticketId']);
-}
-else if ($function == "saveSummary")
-{
-    Validator::validateManager();
-    echo $ticketController->saveSummary($_POST['summary'], $_POST['ticketId']);
-}
-else 
-{
-    Library::notFoundMessage();
+    http_response_code(500);
 }
