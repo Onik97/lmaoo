9a109f812daca082bb4506604f1781ccc711af3a
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-03-05 18:05:29 +0000

    33 - Added routes for CRU feature

--- src/Feature/target.php
+++ src/Feature/target.php
@@ -3,7 +3,10 @@
 try
 {
     if (!Validator::validateUserLoggedIn()) { http_response_code(401); return; }
-    RouteController::Post("loadFeatures", Validator::validateDeveloper(), 'FeatureController::loadFeatures', [@$_POST['projectId']]);
+    RouteController::Post("loadActiveFeatures", Validator::validateDeveloper(), 'FeatureController::loadActiveFeatures', [@$_POST['projectId']]);
+    RouteController::Post("loadInactiveFeatures", Validator::validateDeveloper(), 'FeatureController::loadInactiveFeatures', [@$_POST['projectId']]);
+    RouteController::Post("activateFeature", Validator::validateDeveloper(), 'FeatureController::activateFeature', [@$_POST['projectId']]);
+    RouteController::Post("deactivateFeature", Validator::validateDeveloper(), 'FeatureController::deactivateFeature', [@$_POST['projectId']]);
     RouteController::Post("checkFeatureExistance", Validator::validateDeveloper(), 'FeatureController::featureExistance', [@$_POST['featureName'], @$_POST['projectId']]);
     RouteController::Post("createFeature", Validator::validateManager(), 'FeatureController::createFeature', [@$_POST['featureName'], @$_POST['projectId']]);
     Validator::ThrowNotFound();
