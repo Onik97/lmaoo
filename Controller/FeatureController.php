d60e98548477a24a7552366acb8c33daa6fcb14a
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-03-05 18:04:35 +0000

    33 - Added CRU functions for feature

--- Controller/FeatureController.php
+++ Controller/FeatureController.php
@@ -2,12 +2,39 @@
 
 class FeatureController
 {
-    public static function loadFeatures($projectId)
+    public static function loadActiveFeatures($projectId)
     {
         $pdo = Library::logindb();
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-        $stmt = $pdo->prepare("SELECT * FROM feature WHERE projectId = ?");
+        $stmt = $pdo->prepare("SELECT * FROM feature WHERE projectId = ? AND active = 1");
         $stmt->execute([$projectId]);
+        return $stmt->fetchAll();
+    }
+
+    public static function loadInactiveFeatures($projectId)
+    {
+        $pdo = Library::logindb();
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+        $stmt = $pdo->prepare("SELECT * FROM feature WHERE projectId = ? AND active = 0");
+        $stmt->execute([$projectId]);
+        return $stmt->fetchAll();
+    }
+
+    public static function activateFeature($featureId)
+    {
+        $pdo = Library::logindb();
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+        $stmt = $pdo->prepare("UPDATE feature SET active = 1 WHERE featureId = ?");
+        $stmt->execute([$featureId]);
+        return $stmt->fetchAll();
+    }
+
+    public static function deactivateFeature($featureId)
+    {
+        $pdo = Library::logindb();
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+        $stmt = $pdo->prepare("UPDATE feature SET active = 0 WHERE featureId = ?");
+        $stmt->execute([$featureId]);
         return $stmt->fetchAll();
     }
 
