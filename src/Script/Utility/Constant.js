48fd0eaefc570144dacd641eed552da89dc3fc69
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-03-16 12:49:16 +0000

    40 - Added potential classes

---
+++ src/Script/Utility/Constant.js
@@ -0,0 +1,27 @@
+export default class Constant {
+
+    // User Constants
+    static USER_PASSWORD_MINIMUM_LENGTH = 5;
+
+    
+    // Project Constants
+    
+    
+    
+    // Manager Constants
+
+
+    
+    
+    
+    // Ticket Constants
+
+    
+    
+    
+    
+    // Comment Constants
+    
+
+
+}
