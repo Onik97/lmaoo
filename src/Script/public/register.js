c34bcaa501c00c2804f2a38c00417e029693f6ac
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-03-16 12:48:34 +0000

    40 - Renamed 'src' folder to 'public

--- src/Script/public/register.js
+++ src/Script/public/register.js
@@ -1,4 +1,5 @@
 import RegisterController from "../Controller/RegisterController.js";
+import Constant from "../Utility/Constant.js";
 import DataTypeUtility from "../Utility/DataTypeUtility.js";
 import Validator from "../Utility/Validator.js";
 
@@ -9,7 +10,7 @@ $("#registerForm").submit(function() {
     let error = [];
 
     if (registerData.password1 != registerData.password2) error.push("Password does not match!");
-    if (!Validator.validateMinimumLength(registerData.password1, 5)) error.push("Password must be at least 6 characters long");
+    if (!Validator.validateMinimumLength(registerData.password1, Constant.USER_PASSWORD_MINIMUM_LENGTH)) error.push("Password must be at least 6 characters long");
     if (!Validator.validateNumber(registerData.password1)) error.push("Password must have a number");
     if (!Validator.validateSpecialCharacter(registerData.password1)) error.push("Password must have a special character");
     
@@ -19,6 +20,7 @@ $("#registerForm").submit(function() {
     return error.length == 0 && $("#usernameMessage").html() == "" ? true : false;
 });
 
+// Checks for Username Dupliates
 $("#usernameRegister").keyup(async function() {
     let usernameDuplication = await RegisterController.validateUsername($(this).val());
 
