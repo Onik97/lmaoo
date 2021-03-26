dda123bbfca58ffebb7b62ba5a0dc7f7897fb3e3
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-03-26 15:37:31 +0000

    36 - Added Constants for validation

--- src/Script/Utility/Constant.js
+++ src/Script/Utility/Constant.js
@@ -1,27 +1,21 @@
 export default class Constant {
-
     // User Constants
-    static USER_PASSWORD_MINIMUM_LENGTH = 5;
+    static USER_PASSWORD_MIN = 5;
+    static USER_USERNAME_MAX = 15;
+    static USER_FORENAME_MAX = 15;
+    static USER_SURNAME_MAX = 15;
 
-    
     // Project Constants
+    static PROJECT_NAME_MAX = 20;
+    static PROJECT_STATUS_MAX = 20
     
-    
-    
-    // Manager Constants
-
-
-    
-    
+    // Feature Constants
+    static FEATURE_NAME_MAX = 50;
     
     // Ticket Constants
-
-    
-    
-    
+    static TICKET_SUMMARY_MAX = 50;
+    static TICKET_PROGRESS_MAX = 20; // May make a seperate enum for this
     
     // Comment Constants
-    
-
-
+    static COMMENT_CONTENT_MAX = 255;
 }
