f3713c7407d5b7a768b2d6f0923cfca329262f05
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-03-28 13:51:27 +0100

    43 - Brought back loadProgress to allow real time change on progress

--- src/Script/ticketController.js
+++ src/Script/ticketController.js
@@ -12,6 +12,26 @@ function loadProgressBtn()
 
 }
 
+function loadProgress() 
+{
+    var ticketId = new URL(window.location.href).searchParams.get("ticketId");
+    var data = new FormData();
+    data.append('function', "loadProgress");
+    data.append('ticketId', ticketId);
+
+    axios.post('../Ticket/target.php', data).then((res) => 
+    {
+        $("#ticketProgress").html(res.data);
+        switch(res.data)
+        {
+            case "Open": $("#changeProgressBtn").html("Change progress to 'In Progress'"); break;
+            case "In Progress": $("#changeProgressBtn").html("Change progress to 'In Automation'"); break;
+            case "In Automation": $("#changeProgressBtn").html("Change progress to 'Complete'"); break;
+            case "Complete": $("#changeProgressBtn").html("Re-open ticket"); break;
+        }
+    });
+}
+
 function changeProgress()
 {
     var ticketId = new URL(window.location.href).searchParams.get("ticketId");
@@ -34,7 +54,8 @@ function changeProgress()
     axios.post('../Ticket/target.php', data)
     .then(()=> 
     { 
-        overHang("success", `Status change to '${progress}' successfully!`); 
+        overHang("success", `Status change to '${progress}' successfully!`);
+        loadProgress();
         loadProgressBtn();
         loadDates();
     });
