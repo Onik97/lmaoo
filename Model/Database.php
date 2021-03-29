31e3f19716be4cc00b74f61d22c4b17bbf142430
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-03-29 20:14:02 +0100

    52 - Created Database Class Model

---
+++ Model/Database.php
@@ -0,0 +1,95 @@
+<?php if(!defined('PHPUNIT_COMPOSER_INSTALL')) include_once(__DIR__ . "/../includes/autoloader.inc.php");
+
+class Database
+{
+    function __construct()
+	{
+        $this->query = null;
+        $this->parameters = null;
+    }
+
+    public function query($sql)
+    {
+        $this->query = $sql;
+        return $this;
+    }
+
+    function parameters($parameters)
+    {
+        $this->parameters = $parameters;
+        return $this;
+    }
+
+    function fetchAll()
+    {
+        $config = include(__DIR__ . "/../config.php");
+        try 
+        {
+            $pdo = new PDO("mysql:host={$config['db_host']};dbname={$config['db_table']}", $config['db_username'],  $config['db_password']);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+            $stmt = $pdo->prepare($this->query);
+            $stmt->execute($this->parameters);
+            $pdo = null; // Closes Connection
+            return $stmt->fetchAll();
+        }
+        catch(PDOException $e)
+		{
+			die("ERROR ID: 102");
+		}
+    }
+
+    function fetchColumn()
+    {
+        $config = include(__DIR__ . "/../config.php");
+        try 
+        {
+            $pdo = new PDO("mysql:host={$config['db_host']};dbname={$config['db_table']}", $config['db_username'],  $config['db_password']);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+            $stmt = $pdo->prepare($this->query);
+            $stmt->execute($this->parameters);
+            $pdo = null; // Closes Connection
+            return $stmt->fetchColumn();
+        }
+        catch(PDOException $e)
+		{
+			die("ERROR ID: 102");
+		}
+    }
+
+    
+    function fetchObject()
+    {
+        $config = include(__DIR__ . "/../config.php");
+        try 
+        {
+            $pdo = new PDO("mysql:host={$config['db_host']};dbname={$config['db_table']}", $config['db_username'],  $config['db_password']);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+            $stmt = $pdo->prepare($this->query);
+            $stmt->execute($this->parameters);
+            $pdo = null; // Closes Connection
+            return $stmt->fetchObject();
+        }
+        catch(PDOException $e)
+		{
+			die("ERROR ID: 102");
+		}
+    }
+
+    function rowCount()
+    {
+        $config = include(__DIR__ . "/../config.php");
+        try 
+        {
+            $pdo = new PDO("mysql:host={$config['db_host']};dbname={$config['db_table']}", $config['db_username'],  $config['db_password']);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+            $stmt = $pdo->prepare($this->query);
+            $stmt->execute($this->parameters);
+            $pdo = null; // Closes Connection
+            return $stmt->rowCount();
+        }
+        catch(PDOException $e)
+		{
+			die("ERROR ID: 102");
+		}
+    }
+}
