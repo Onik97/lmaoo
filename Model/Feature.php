1af893d163aedd193ece9976a16b364c4ec96112
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-04-01 16:54:23 +0100

    52 - Adapted Feature Class

--- Model/Feature.php
+++ Model/Feature.php
@@ -5,37 +5,37 @@ class Feature extends Database
     public function getActiveFeature($projectId)
     {
         $sql = "SELECT * FROM feature WHERE projectId = ? AND active = 1";
-        return $this->query($sql)->parameters([$projectId])->fetchAll();
+        return self::db()::query($sql)->parameters([$projectId])->fetchAll();
     }
 
     public function getInactiveFeature($projectId)
     {
         $sql = "SELECT * FROM feature WHERE projectId = ? AND active = 0";
-        return $this->query($sql)->parameters([$projectId])->fetchAll();
+        return self::db()::query($sql)->parameters([$projectId])->fetchAll();
     }
 
     public function activateFeature($featureId)
     {
         $sql = "UPDATE feature SET active = 1 WHERE featureId = ?";
-        return $this->query($sql)->parameters([$featureId])->fetchAll();
+        return self::db()::query($sql)->parameters([$featureId])->fetchAll();
     }
 
     public function deactivateFeature($featureId)
     {
         $sql = "UPDATE feature SET active = 0 WHERE featureId = ?";
-        return $this->query($sql)->parameters([$featureId])->fetchAll();
+        return self::db()::query($sql)->parameters([$featureId])->fetchAll();
     }
 
     public function doesFeatureExists($featureName, $projectId)
     {
         $sql = "SELECT name FROM feature WHERE name = ? AND projectId = ?";
-        return $this->query($sql)->parameters([$featureName, $projectId])->rowCount()
+        return self::db()::query($sql)->parameters([$featureName, $projectId])->rowCount()
         ? true : false;
     }
 
     public function createFeature($featureName, $projectId)
     {
         $sql = "INSERT INTO feature (name, projectId) VALUES (?, ?)";
-        $this->query($sql)->parameters([$featureName, $projectId])->exec();
+        self::db()::query($sql)->parameters([$featureName, $projectId])->exec();
     }
 }
