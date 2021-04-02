96c0b6b2764219e24abecf2af364adc5d4d768e1
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-04-02 22:17:57 +0100

    52 - Created Interface for Models

---
+++ Model/IModal.php
@@ -0,0 +1,12 @@
+<?php
+
+interface IModal 
+{
+    public static function create(array $data);
+
+    public static function withId($id, $columns);
+
+    public static function update($id, array $data);
+    
+    public static function delete($id);
+}
