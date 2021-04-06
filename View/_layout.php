9b7aac9ff1023335c5a9b27866f3e27797be1215
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-04-06 17:43:52 +0100

    56 - Tidied Layout

--- View/_layout.php
+++ View/_layout.php
@@ -1,31 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php include_once(__DIR__ . "/../includes/autoloader.inc.php"); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.css" rel="stylesheet">
     <link rel="stylesheet" href="../Css/notifications.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-
-    <script> <?php
-    $userController = new UserController();
-    if(isset($_SESSION['userLoggedIn'])) 
-    {
-    $userLoggedIn = unserialize($_SESSION['userLoggedIn']);
-    
-    echo "const userId = '" . $userLoggedIn->userId . "'\n";
-    echo "const userForename = '" . $userLoggedIn->forename. "'\n";
-    echo "const userSurname = '" . $userLoggedIn->surname. "'\n";
-    echo "const userUsername = '" . $userLoggedIn->username. "'\n";
-    echo "const userLevel = '" . $userLoggedIn->level. "'\n";
-
-    if ($userLoggedIn->level > 1)
-    {
-        echo "const users = " . json_encode($userController->getActiveUsers()) . "\n";
-    }
-    } 
-    ?> </script>
 </head>
 <body>
 
@@ -50,6 +30,27 @@
 <script src="../Script/navBar.js"></script>
 
 <script>
+const darkmode =  new Darkmode();
+
+<?php
+include_once(__DIR__ . "/../includes/autoloader.inc.php");
+$userController = new UserController();
+if(isset($_SESSION['userLoggedIn'])) 
+{
+  $userLoggedIn = unserialize($_SESSION['userLoggedIn']);
+  
+  echo "const userId = '" . $userLoggedIn->userId . "'\n";
+  echo "const userForename = '" . $userLoggedIn->forename. "'\n";
+  echo "const userSurname = '" . $userLoggedIn->surname. "'\n";
+  echo "const userUsername = '" . $userLoggedIn->username. "'\n";
+  echo "const userLevel = '" . $userLoggedIn->level. "'\n";
+
+  if ($userLoggedIn->level > 1)
+  {
+      echo "const users = " . json_encode($userController->getActiveUsers()) . "\n";
+  }
+}
+?>
 
 function overHang(type, message)
 {
@@ -63,8 +64,6 @@ function overHang(type, message)
   
   myNotification({ message: message });
 }
-
-const darkmode =  new Darkmode();
 
 </script>
 <?php echo $script; ?>
