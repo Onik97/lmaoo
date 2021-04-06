8b613310a2bd54d3719e6ccdbd5e120c22d4abd4
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-04-06 17:50:32 +0100

    56 - RenderController Clean up

--- Controller/RenderController.php
+++ Controller/RenderController.php
@@ -8,13 +8,9 @@ class RenderController
         $userLoggedIn == null ? $router->render("aboutUs", "home") : $router->render("dashboard", "home");
     }
 
-    public static function login(Router $router) 
-	{
-		$router->render("login", "login");
-	}
-
-	public static function register(Router $router)
-	{
-		$router->render("register", "register");
-	}
+    public static function login(Router $router) { $router->render("login", "login"); }
+	public static function register(Router $router) { $router->render("register", "register"); }
+	public static function manager(Router $router) { $router->render("manager", "manager"); }
+	public static function project(Router $router) { $router->render("project", "project"); }
+	public static function admin(Router $router) { $router->render("admin", "admin"); }
 }
