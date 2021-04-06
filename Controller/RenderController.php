ae5bcc4029f7b63f2c0a065d1eff4826e8db64da
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-04-06 17:50:59 +0100

    56 - RenderController renders dropdown items on navbar

--- Controller/RenderController.php
+++ Controller/RenderController.php
@@ -13,4 +13,21 @@ class RenderController
 	public static function manager(Router $router) { $router->render("manager", "manager"); }
 	public static function project(Router $router) { $router->render("project", "project"); }
 	public static function admin(Router $router) { $router->render("admin", "admin"); }
+
+	public static function renderDropdownItems($userLoggedIn)
+	{
+		$userLoggedIn = unserialize($userLoggedIn);
+		if($userLoggedIn == null)
+		{
+			echo "<a class='dropdown-item' id='registerNav' href='/register'>Register</a>";
+			echo "<a class='dropdown-item' id='loginNav' href='/login'>Login</a>";
+		}
+		else
+		{
+			if ($userLoggedIn->level > 1) echo "<a class='dropdown-item' id='managerNav' href='/manager'>Manager</a>"; 
+			echo "<a class='dropdown-item' id='editAccountNav' data-toggle='modal' data-target='#view-modal' role='button'>Edit Account</a>";
+			echo "<a class='dropdown-item' id='logoutNav' href='/logout'>Logout</a>";
+			if($userLoggedIn->level > 3) echo "<a class='dropdown-item' id='adminNav' href='/admin'>Admin</a>";
+		}
+	}
 }
