b572d6078f68fcaeb7ebe521c5f35a52ce021ab4
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-04-06 16:23:40 +0100

    56 - Index now handles routes

--- src/index.php
+++ src/index.php
@@ -1,1 +1,16 @@
-<?php header("Location: Home/index.php"); ?>
+<?php if(!defined('PHPUNIT_COMPOSER_INSTALL')) include_once(__DIR__ . "/../includes/autoloader.inc.php");
+
+$router = new Router();
+
+// Render Views
+
+$router->get("/", [RenderController::class, "index"]);
+$router->get("/login", [RenderController::class, "login"]);
+$router->get("/register", [RenderController::class, "register"]);
+
+// User Routes
+
+$router->post("/login", [UserController::class, "standardLogin"]);
+
+
+$router->resolve();
