5a6ecd3be47d8cd22f6e0812644e4d8ace177493
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-04-12 11:47:22 +0100

    45 - Added another model method for Feature

--- Model/Feature.php
+++ Model/Feature.php
@@ -14,6 +14,12 @@ class Feature extends Database implements IModel
         return self::db()::query($sql)::parameters([$featureId])::fetchObject();
     }
 
+    public static function withProjectId($projectId, $columns = null)
+    {
+        $sql = $columns == null ? "SELECT * FROM feature WHERE projectId = ?" : "SELECT $columns FROM feature WHERE projectId = ?";
+        return self::db()::query($sql)::parameters([$projectId])::fetchAll();
+    }
+
     public static function update($featureId, array $data)
     {
         $sql = Library::arrayToUpdateQuery("feature", $data);
