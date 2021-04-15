430038abf2ace87de3cd8de4fe719dc99ef68527
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2021-04-15 17:52:00 +0100

    47 - made temp code for testing

--- src/Script/public/project.js
+++ src/Script/public/project.js
@@ -2,3 +2,5 @@ import Navbar from "../public/navbar.js";
 import FragmentUrl from "../Utility/FragmentCRUD.js";
 
 $(document).ready(() => { Navbar.projectActiveTab(); });
+
+$('fragmentCreate').on("click", "#fragmentCreate", function(){ alert('worked')})
