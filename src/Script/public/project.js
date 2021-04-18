9e33080c3e8c610364fd0199d2ef8142294fad7d
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2021-04-18 14:22:07 +0100

    47 - removed testing code

--- src/Script/public/project.js
+++ src/Script/public/project.js
@@ -2,11 +2,3 @@ import Navbar from "../public/navbar.js";
 import FragmentUrl from "../Utility/FragmentCRUD.js";
 
 $(document).ready(() => { Navbar.projectActiveTab(); });
-
-$('#fragmentCreate').click(function(){ FragmentUrl.createFragment() });
-
-$('#fragmentRead').click(function(){ FragmentUrl.readFragment() });
-
-$('#fragmentUpdate').click(function(){ FragmentUrl.updateFragment() });
-
-$('#fragmentDelete').click(function(){ FragmentUrl.deleteFragment() });
