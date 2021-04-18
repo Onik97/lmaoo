167a95c8f78048c16c0ca83e8646e87b09e4ba3d
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2021-04-18 16:37:27 +0100

    47 - fixed readFragment errors

--- src/Script/Utility/Fragment.js
+++ src/Script/Utility/Fragment.js
@@ -5,10 +5,11 @@ export default class Fragment
         window.location.hash = `#${currentLocationHash}`;
     }
 
-    static readFragment(currentLocationHash)
+    static readFragment()
     {
         if (window.location.hash == '') { return('No Url Fragment loaded')}
-        return(`#${currentLocationHash}`);
+        console.log(window.location.hash);
+        return(window.location.hash);
     }
 
     static updateFragment(currentLocationHash)
