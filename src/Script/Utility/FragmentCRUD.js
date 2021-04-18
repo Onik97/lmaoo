941f1ff02a2b24390a90ac99874075195063f29b
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2021-04-18 15:59:15 +0100

    47 - removed unused code

--- src/Script/Utility/FragmentCRUD.js
+++ src/Script/Utility/FragmentCRUD.js
@@ -1,29 +1,20 @@
 export default class FragmentUrl
 {
-    constructor(){
-    this.urlFragment = 'unknown';
-    }
-
     static createFragment(currentLocationHash)
     {
         window.location.hash = currentLocationHash;
-        return this.urlFragment = currentLocationHash;
     }
 
-    static readFragment()
     {
-        alert(this.urlFragment);
     }
 
     static updateFragment(currentLocationHash)
     {
         window.location.hash = currentLocationHash
-        return this.urlFragment = currentLocationHash;
     }
 
     static deleteFragment()
     {
         window.location.hash = ''
-        return this.urlFragment = 'No url hash location'
     }
 }
