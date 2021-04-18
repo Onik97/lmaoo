701f92b2f07691aee7dbe78c08e66914709465ee
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2021-04-18 16:21:46 +0100

    47 - corrected file path

--- src/Script/public/project.js
+++ src/Script/public/project.js
@@ -1,4 +1,4 @@
 import Navbar from "../public/navbar.js";
-import FragmentUrl from "../Utility/FragmentCRUD.js";
+import Fragment from "../Utility/Fragment.js";
 
 $(document).ready(() => { Navbar.projectActiveTab(); });
