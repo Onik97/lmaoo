d18b2f0b8dcd2b83e6eb34adc1f278bd6aa4597a
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2021-04-18 16:21:18 +0100

    47 - fixed previous code logic

--- src/Script/Utility/Fragment.js
+++ src/Script/Utility/Fragment.js
@@ -2,17 +2,22 @@ export default class Fragment
 {
     static createFragment(currentLocationHash)
     {
+        window.location.hash = `#${currentLocationHash}`;
     }
 
     static readFragment(currentLocationHash)
     {
+        if (window.location.hash == '') { return('No Url Fragment loaded')}
+        return(`#${currentLocationHash}`);
     }
 
     static updateFragment(currentLocationHash)
     {
+        window.location.hash = `#${currentLocationHash}`;
     }
 
     static deleteFragment()
     {
+        window.location.hash = ''
     }
 }
