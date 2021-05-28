c41db95fea244854947d89ad03f087d3a30e7641
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-05-28 19:36:05 +0100

    58 - Fixed Comment Class Model

--- App/Model/Comment.php
+++ App/Model/Comment.php
@@ -3,9 +3,8 @@ namespace Lmaoo\Model;
 
 use Lmaoo\Core\Database;
 use Lmaoo\Utility\Library;
-use IModel;
 
-class Ticket extends Database implements IModel
+class Comment extends Database implements IModel
 {
     public static function create(array $data)
     {
@@ -15,7 +14,7 @@ class Ticket extends Database implements IModel
 
     public static function withId($commentId, $columns = null)
     {
-        $sql = $columns == null ? "SELECT * FROM project WHERE projectId = ?" : "SELECT $columns FROM project WHERE projectId = ?";
+        $sql = $columns == null ? "SELECT * FROM comment WHERE commentId = ?" : "SELECT $columns FROM comment WHERE commentId = ?";
         return self::db()::query($sql)::parameters([$commentId])::fetchObject();
     }
 
@@ -34,7 +33,7 @@ class Ticket extends Database implements IModel
 
     public static function delete($commentId)
     {
-        $sql = "UPDATE project SET active = 0 WHERE commentId = ?";
+        $sql = "DELETE FROM comment WHERE commentId = ?";
         self::db()::query($sql)::parameters([$commentId])::exec();
     }
 }
