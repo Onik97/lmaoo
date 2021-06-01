c63c99ec2c93eb056664ad051a2eb3069a751bec
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-06-01 22:46:31 +0100

    48 - Created ProjectAccess Model

---
+++ App/Model/ProjectAccess.php
@@ -0,0 +1,28 @@
+<?php
+namespace Lmaoo\Model;
+
+use Lmaoo\Core\Database;
+use Lmaoo\Utility\Library;
+
+class ProjectAccess extends Database
+{
+    public static function create(array $data) 
+    {
+        $sql = Library::arrayToInsertQuery("projectAccess", $data);
+        self::db()::query($sql)::parameters([])::exec();
+    }
+
+    public static function Navbar(string $userId)
+    {
+        $sql = "SELECT DISTINCT p.projectId, p.name, p.owner FROM projectAccess pa RIGHT JOIN project p ON pa.projectId = p.projectId 
+                WHERE pa.allowAccess = 1 AND pa.userId = ? OR p.owner = ?";
+        return self::db()::query($sql)::parameters([$userId, $userId])::fetchAll();
+    }
+
+    public static function withManagerAccess(string $userId)
+    {
+        $sql = "SELECT pa.userId, pa.projectId, p.name, p.status FROM projectAccess pa INNER JOIN project p ON pa.projectId = p.projectId 
+                WHERE pa.managerAccess = 1 AND pa.userId = ?";
+        return self::db()::query($sql)::parameters([$userId])::fetchAll();
+    }
+}
