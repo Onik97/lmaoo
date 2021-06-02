492339fa4bbfed8328d4f4571e94aaffb6aaeb88
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2021-06-02 15:13:50 +0100

    69 - added path to ProjectController for use

--- App/src/index.php
+++ App/src/index.php
@@ -2,6 +2,7 @@
 
 if (session_status() == PHP_SESSION_NONE) session_start();
 
+use Lmaoo\Controller\ProjectController;
 use Lmaoo\Core\Router;
 use Lmaoo\Core\Render;
 use Lmaoo\Controller\UserController;
