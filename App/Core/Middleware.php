f57538b090cbb9ad2abc53fa8e663704d1c473da
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-06-04 22:29:34 +0100

    48 - Middleware now has verifyUser

--- App/Core/Middleware.php
+++ App/Core/Middleware.php
@@ -3,5 +3,22 @@ namespace Lmaoo\Core;
 
 class Middleware
 {
+    public static function verifyUser($router, $userLevel)
+    {
+        $userLoggedIn = $_SESSION["userLoggedIn"] ?? null;
+        
+        if ($userLoggedIn == null)
+        {
+            $router->trigger404();
+            http_response_code(404);
+            return exit();
+        }
+         
+        if ($userLoggedIn->level < $userLevel)
+        {
+            $router->trigger404();
+            http_response_code(404);
+            return exit();
+        }
     }
 }
