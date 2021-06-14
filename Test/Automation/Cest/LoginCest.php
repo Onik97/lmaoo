b7ed05672db1cd63d7261f83978c122bc3e2bd6f
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-06-14 20:42:28 +0100

    72 - Made start with Automation for Marcus

---
+++ Test/Automation/Cest/LoginCest.php
@@ -0,0 +1,21 @@
+<?php
+
+class LoginCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+        
+    }
+
+    public function LoginSuccess(AcceptanceTester $I)
+    {
+    }
+
+    public function LoginFailed(AcceptanceTester $I)
+    {
+    }
+
+    public function LoginInactive(AcceptanceTester $I)
+    {
+    }
+}
--- Test/Automation/Features/Login.feature
+++
@@ -1,4 +0,0 @@
-Feature: Login 
-
-  Scenario: I want to sign in as a user
-    Given I go to the Login Page
