66f580806a1068b27c87de896ae2804996b9cdc1
MarcusBeddall <56123521+MarcusBeddall@users.noreply.github.com>
2021-06-22 21:36:48 +0100

    71 - Login automation completed

--- Test/Automation/Tools/_support/Page/Acceptance/Login.php
+++ Test/Automation/Tools/_support/Page/Acceptance/Login.php
@@ -7,11 +7,21 @@ class Login
      * @var \AcceptanceTester;
      */
     protected $acceptanceTester;
-    // Selectors here -> Also remove this comment
+    
+    public $loginTextbox = "#usernameLogin";
+    public $passwordTextbox = "#passwordLogin";
+    public $loginButton = "body > main > div > div > div > div.col-md-6.text-center > div > form > button";
+    public $registerHyperlink = "body > main > div > div > div > div.col-md-6.text-center > div > form > div.form-group.mt-3 > a";
 
     public function __construct(\AcceptanceTester $I)
     {
         $this->acceptanceTester = $I;
     }
 
+    public function login($username, $password)
+    {
+        $this->acceptanceTester->fillField($this->loginTextbox, $username);
+        $this->acceptanceTester->fillField($this->passwordTextbox, $password);
+        $this->acceptanceTester->click($this->loginButton);
+    }
 }
