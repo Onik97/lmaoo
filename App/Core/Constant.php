ea197f710d285c2372c0f5978747ceb696f1cb90
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-06-25 22:07:59 +0100

    Hotfix done

--- App/Core/Constant.php
+++ App/Core/Constant.php
@@ -4,7 +4,7 @@ namespace Lmaoo\Core;
 class Constant 
 {
     // Database Columns
-    public static array $USER_COLUMNS = ["userId", "username", "password", "forename", "surname", "level", "isActive", "picture", "github_id", "github_accessToken"];
+    public static array $USER_COLUMNS = ["userId", "username", "password", "forename", "surname", "level", "isActive", "picture", "darkMode", "github_id", "github_accessToken"];
     public static array $COMMENT_COLUMNS = ["commentId", "commentContent", "commentCreated", "ticketId", "userId"];
     public static array $FEATURE_COLUMNS = ["featureId", "name", "projectId", "active"];
     public static array $PROJECT_COLUMNS = ["projectId", "name", "status", "owner", "active"];
