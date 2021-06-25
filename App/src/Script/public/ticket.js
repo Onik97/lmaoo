4b9378142773d16c019e65f55e3d086ac01c7694
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-06-26 00:40:24 +0100

    44 - Summernote now loads on Ticket Page

--- App/src/Script/public/ticket.js
+++ App/src/Script/public/ticket.js
@@ -1,4 +1,5 @@
 import Navbar from "../public/navbar.js";
-
+import SummernoteWrapper from "../Utility/SummernoteWrapper.js";
 
 Navbar.projectActiveTab();
+SummernoteWrapper.Load(".createComment", "Enter your comment here, Shift + Enter to save");
