2f18c3f337cd7c249422289efa230e5935c20e40
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-06-26 00:39:52 +0100

    44 - CRUD for Summernote

--- App/src/Script/Utility/SummernoteWrapper.js
+++ App/src/Script/Utility/SummernoteWrapper.js
@@ -1,3 +1,31 @@
 export default class SummernoteWrapper {
-    
+    static Load(selector, placeholder) {
+        $(selector).summernote({
+            placeholder: placeholder, 
+            height: 125,
+            toolbar: [ ['style', ['bold', 'italic', 'underline', 'clear']], ['font', ['strikethrough' ]], ['para', ['ul', 'ol']] ],
+            popover: { image: [], link: [], air: [] }
+        });
+        $('.note-statusbar').hide();
+        return this;
+    }
+
+    static onKeyDown(selector, callback) {
+        $(selector).on('summernote.keydown', (we, e) => callback());
+        return this;
+    }
+
+    static getValue(selector) {
+        return $(selector).summernote('code');
+    }
+
+    static setValue(selector, value) {
+        $(selector).summernote('code', value);
+        return this;
+    }
+
+    static Close(selector) {
+        $(selector).summernote("destroy");
+        return this;
+    }
 }
