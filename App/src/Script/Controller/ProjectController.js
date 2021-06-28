7ae085f541683ec2ba8fb1aee240aad6fafc337a
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2021-06-28 21:16:03 +0100

    86 - made a temporary createFeature function

--- App/src/Script/Controller/ProjectController.js
+++ App/src/Script/Controller/ProjectController.js
@@ -2,4 +2,15 @@ import axios from "../Utility/AxiosWrapper.js";
 
 export default class Project 
 {
+    static async createFeature()
+    {
+        let projectId = new URL(window.location.href).searchParams.get("projectId");
+        let name = $("#projectName").text();
+
+        let data = [{"name" : name, "projectId" : projectId}]
+        let endpoint = "/feature/"
+
+        let result = await axios.Post(endpoint, data);
+        return result;
+    }
 }
