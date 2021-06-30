3d3c413834c317c3d83a6442d3bb785573eb1b77
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-06-30 23:21:12 +0100

    84 - WebComponents now has comments

--- App/src/Script/Utility/WebComponent.js
+++ App/src/Script/Utility/WebComponent.js
@@ -1,4 +1,31 @@
 export default class WebComponent {
+    static Comment(selector, comment) {
+        let { commentId, commentContent, commentCreated, forename, surname, picture } = comment; 
+        $(selector).append(`
+                <div class='row'>
+                    <div class='col-1'>
+                        <div id='commentThumbnail'>
+                            <img class='profilePicture mt-1' src=${picture}'>
+                        </div>
+                    </div>
+
+                    <div class='col-8'>
+                        <div id='commentBody' class='mt-2 ml-2'>
+                            <h6>${forename} ${surname}</h6>
+                            <span>${commentCreated}</span>
+                        </div>
+
+                        <div id='mainComment' class='ml-2'>${commentContent}</div>
+                    </div>
+                
+                    <div class='col-2 mt-2 ml-5' id='commentActions'>
+                        <img class='CommentImages' src='/Images/trash.svg' data-toggle='modal' data-target='#ticketPageModal' value='${commentId}' role='button'>
+                        <img class='CommentImages' src='/Images/pencilsquare.svg' value='${commentId}' role='button'>
+                    </div>
+                </div>
+            `);
+    }
+
     static Assignee(selector, assignees) {
         $(selector).html("<option value='0' selected disabled>Select user</option>"); // To avoid duplicates
         assignees.forEach(assignee => {
