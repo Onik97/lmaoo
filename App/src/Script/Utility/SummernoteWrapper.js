a39a90594057f08c6144fdbc3d455f60cf708b83
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-06-30 23:20:41 +0100

    84 - Summernote class no longer static

--- App/src/Script/Utility/SummernoteWrapper.js
+++ App/src/Script/Utility/SummernoteWrapper.js
@@ -1,30 +1,42 @@
 export default class SummernoteWrapper {
-    static Load(selector, placeholder) {
+    constructor(selector, placeholder, simple = false) {
+        this.selector = selector;
         $(selector).summernote({
             placeholder: placeholder, 
             height: 125,
-            toolbar: [ ['style', ['bold', 'italic', 'underline', 'clear']], ['font', ['strikethrough' ]], ['para', ['ul', 'ol']] ],
+            toolbar: simple == false 
+                     ? [ ['style', ['bold', 'italic', 'underline', 'clear']], ['font', ['strikethrough' ]], ['para', ['ul', 'ol']] ]
+                     : [] ,
             popover: { image: [], link: [], air: [] }
         });
         $('.note-statusbar').hide();
+        this.initalValue = this.getValue();
+        return this;
+    }
+    
+    onKeyDown(saveCallback, cancelCallback = null) {
+        $(this.selector).on('summernote.keydown', (we, e) => {
+            if(e.ctrlKey && e.code == "Enter") {
+                this.value = $(this.selector).summernote('code');
+                e.preventDefault();
+                saveCallback();
+            }
+            if (cancelCallback != null) { 
+                if(e.code == "Escape") cancelCallback(this.initalValue); 
+            }
+        });
+    }
+
+    getValue() {
+        return $(this.selector).summernote('code');
+    }
+
+    setValue(value) {
+        $(this.selector).summernote('code', value);
         return this;
     }
 
-    static onKeyDown(selector, callback) {
-        $(selector).on('summernote.keydown', (we, e) => callback());
-        return this;
-    }
-
-    static getValue(selector) {
-        return $(selector).summernote('code');
-    }
-
-    static setValue(selector, value) {
-        $(selector).summernote('code', value);
-        return this;
-    }
-
-    static Close(selector) {
+    Close(selector) {
         $(selector).summernote("destroy");
         return this;
     }
