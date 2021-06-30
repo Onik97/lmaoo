e8d645b5a7b84eb5c449861ea9e6c5198783e794
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-06-30 13:54:53 +0100

    84 - Adjusted Table for a cleaner look + Adjusted JS Accordingly

--- App/src/Script/public/admin.js
+++ App/src/Script/public/admin.js
@@ -3,24 +3,23 @@ import AdminController from "../Controller/AdminController.js";
 
 Navbar.accountActiveTab();
 
+// Load users according to what element is selected
 $('#adminSelect').change(async function() {
     $("#admin-table").find("tr:gt(0)").remove();
     let json = await AdminController.getUsers(this.value);
     for (let i = 0; i < json.length; i++)
     {
         let newRow = document.getElementById("admin-table").insertRow(-1);
-        let cell1 = newRow.insertCell(0);
-        let cell2 = newRow.insertCell(1);
-        let cell3 = newRow.insertCell(2);
-        let cell4 = newRow.insertCell(3);
-        let cell5 = newRow.insertCell(4);
-        let cell6 = newRow.insertCell(5);
+        let cell2 = newRow.insertCell(0);
+        let cell3 = newRow.insertCell(1);
+        let cell4 = newRow.insertCell(2);
+        let cell5 = newRow.insertCell(3);
+        let cell6 = newRow.insertCell(4);
 
-        $(cell1).append(document.createTextNode(json[i].userId));
         $(cell2).append(document.createTextNode(json[i].username));
         $(cell3).append(document.createTextNode(json[i].forename));
         $(cell4).append(document.createTextNode(json[i].surname));
         $(cell5).append(document.createTextNode(json[i].level));
-        $(cell6).append($("<i>", { class:"fas fa-user-edit", id: "editUser", "data-toggle": "modal", "data-target": "#adminModal" }).html(""));
+        $(cell6).append($("<i>", { class:"fas fa-user-edit", value: `${json[i].userId}`, "data-toggle": "modal", "data-target": "#adminModal" }));
     }
 });
