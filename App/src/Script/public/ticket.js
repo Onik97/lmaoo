67665ee8caa65e859fa023ad7c0684d5630c0891
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-06-30 23:21:33 +0100

    84 - Comments can now be added + Summernote instance is now being used

--- App/src/Script/public/ticket.js
+++ App/src/Script/public/ticket.js
@@ -1,13 +1,25 @@
 import TicketController from "../Controller/TicketController.js";
 import Navbar from "../public/navbar.js";
-import SummernoteWrapper from "../Utility/SummernoteWrapper.js";
+import axios from "../Utility/AxiosWrapper.js";
+import NotificationWrapper from "../Utility/NotificationWrapper.js";
+import Summernote from "../Utility/SummernoteWrapper.js";
 import WebComponent from "../Utility/WebComponent.js";
 
 Navbar.projectActiveTab();
-SummernoteWrapper.Load(".createComment", "Enter your comment here, Shift + Enter to save");
+let createCommentSummernote = new Summernote(".createComment", "Enter your comment here, CTRL + Enter to save");
 
 // Loads users on Choose Assignee Modal
 $("#chooseAssigneeBtn").click(async function() {
     let assignees = await TicketController.loadAssignees();
     WebComponent.Assignee("#assigneeSelect", assignees);
+});
+
+// Create Comment Summernote
+createCommentSummernote.onKeyDown(async () => {
+    let commentContent = createCommentSummernote.getValue();
+    let ticketId = window.location.href.split('?')[0].split("/").reverse()[0];
+    let result = await axios.Post("/ticket/comment", { commentContent, ticketId });
+    createCommentSummernote.setValue("");
+    WebComponent.Comment("#commentList", result);
+    NotificationWrapper.successMessage("New Comment added Successfully!")
 });
