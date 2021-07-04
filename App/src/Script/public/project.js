c3a624c81fff2efb44acbdf20f608f8b86d68f92
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2021-07-04 07:12:33 +0100

    86 - importing needed classes for function uses

--- App/src/Script/public/project.js
+++ App/src/Script/public/project.js
@@ -1,4 +1,6 @@
 import Navbar from "../public/navbar.js";
 import Fragment from "../Utility/Fragment.js";
+import Project from "../Controller/ProjectController.js";
+import notification from "../Utility/NotificationWrapper.js";
 
 $(document).ready(() => { Navbar.projectActiveTab(); });
