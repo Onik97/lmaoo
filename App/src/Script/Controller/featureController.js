633f9e9c3dda2980f2717897053981d778bc2a41
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2021-07-04 15:47:31 +0100

    86 - Created featureControler, added in all the code from project controller

---
+++ App/src/Script/Controller/featureController.js
@@ -0,0 +1,28 @@
+import axios from "../Utility/AxiosWrapper.js";
+
+export default class Feature
+{
+    static async createFeature(json)
+    {
+        let result = await axios.Post("/feature",json);
+        return result;
+    }
+
+    static async readFeature(featureId)
+    {
+        let result = await axios.Get(`/feature/${featureId}`);
+        return result[0];
+    }
+
+    static async updateFeature(json)
+    {
+        let result = await axios.Put("/feature", json);
+        return result;
+    }
+    
+    static async deleteFeature(featureId)
+    {
+        let result = await axios.Delete("/feature", featureId);
+        return result;
+    }
+}
