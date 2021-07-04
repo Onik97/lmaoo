92188479f6148ce79918e1c4cd5605302c40171a
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2021-07-04 07:15:29 +0100

    86 - createProject Javascript done

--- App/src/Script/public/manager.js
+++ App/src/Script/public/manager.js
@@ -3,3 +3,14 @@ import Project from "../Controller/ProjectController.js";
 import notification from "../Utility/NotificationWrapper.js";
 
 $(document).ready(() => { Navbar.accountActiveTab(); })
+
+$("#createProjectButton").click(async function(){
+    let name = $("#projectName").val();
+    let status = $("#projectStatus").children("option:selected").val();
+    let owner = "";
+    let active = "1"
+
+    let result = await Project.createProject({name, status, owner, active});
+    result == null ? notification.errorMessage("Something went wrong!") : notification.successMessage("User has been updated!");
+    $("#createProjectModal").modal("hide");
+});
