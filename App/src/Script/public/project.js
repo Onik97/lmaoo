894396aba95bfef400f39e624f6df58f48c5405c
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2021-07-04 07:13:20 +0100

    86 - created Global projectId variable for all functions

--- App/src/Script/public/project.js
+++ App/src/Script/public/project.js
@@ -4,3 +4,7 @@ import Project from "../Controller/ProjectController.js";
 import notification from "../Utility/NotificationWrapper.js";
 
 $(document).ready(() => { Navbar.projectActiveTab(); });
+let projectIdData = window.location.href.split('?')[0].split("/").reverse()[0];
+let projectId = Number(projectIdData);
+
+$("#createFeatureButton").click(async function(){
