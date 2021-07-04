9db1cbb31a19d6e9b02560f12842f9137a5b58f6
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2021-07-04 07:14:06 +0100

    86 - createFeature Javascript done

--- App/src/Script/public/project.js
+++ App/src/Script/public/project.js
@@ -8,3 +8,9 @@ let projectIdData = window.location.href.split('?')[0].split("/").reverse()[0];
 let projectId = Number(projectIdData);
 
 $("#createFeatureButton").click(async function(){
+    let name = $("#featureName").val();
+    let result = await Project.createFeature({projectId, name});
+
+    result == null ? notification.errorMessage("Something went wrong!") : notification.successMessage("Feature has been created!");
+    $("#createFeatureModal").modal("hide");
+});
