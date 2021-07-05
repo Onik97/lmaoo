3cc5dfa81846a3407bbba8a676f9dc3462d541ef
Jewell-Micah <imics15@gmail.com>
2021-07-05 12:18:43 +0100

    83 - Profile Page Frontend

--- App/View/profile.php
+++ App/View/profile.php
@@ -1,6 +1,6 @@
 <p id="navBarActive" hidden>profilePage</p>
 
-<h1>Profile Page</h1>
+<h1 class="headings">Profile Page</h1>
     <div class="modal-body">    
         <label>Upload Profile Picture</label>
         <div class="input-group my-2">
@@ -32,6 +32,26 @@
                 <input class="form-control" id="editUsername" name="editUsername" required>
                 <small id="editUsernameMessage" hidden></small> 
             </div>
+
+
+        <h2>Reset Password</h2>
+            <div class="form-group">
+                <label>Current Password</label>
+                <input class="form-control" id="Password" name="Password" required>
+                <small id="PasswordMessage" hidden></small> 
+            </div>
+
+            <div class="form-group">
+                <label>New Password</label>
+                <input class="form-control" id="editPassword" name="editPassword" required>
+                <small id="editPasswordMessage" hidden></small> 
+            </div>
+
+            <div class="form-group">
+                <label>Confirm New Password</label>
+                <input class="form-control" id="confirmPassword" name="confirmPassword" required>
+                <small id="confirmPasswordMessage" hidden></small> 
+            </div>
     </div>
             <input type="hidden" name="function" value="update">
             <input type="hidden" name="editUserId">
@@ -43,4 +63,11 @@
     </div>
 </div>
 </div> 
+</div>
+
+<div class="link">
+    <p1>Linked With Github</p1> &nbsp <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" style=”width:40px;height:20px;">  
+</div>
+<div class="link"> 
+    <p2>Linked With Discord</p2> &nbsp <img src="https://preview.redd.it/s9biyhs4lix61.jpg?auto=webp&s=c600a95eff95b3e9406a8b913c6aa3988b5e3a8b" style=”width:40px;height:20px;">   
 </div>
