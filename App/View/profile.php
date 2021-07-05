4db7af937f166bfeb933a517ca68172d6ba34958
Jewell-Micah <imics15@gmail.com>
2021-07-05 18:42:42 +0100

    83 - edit password modal added

--- App/View/profile.php
+++ App/View/profile.php
@@ -32,25 +32,6 @@
                 <input class="form-control" id="editUsername" name="editUsername" required>
                 <small id="editUsernameMessage" hidden></small> 
             </div>
-
-        <h2>Reset Password</h2>
-            <div class="form-group">
-                <label>Current Password</label>
-                <input class="form-control" id="Password" name="Password" required>
-                <small id="PasswordMessage" hidden></small> 
-            </div>
-
-            <div class="form-group">
-                <label>New Password</label>
-                <input class="form-control" id="editPassword" name="editPassword" required>
-                <small id="editPasswordMessage" hidden></small> 
-            </div>
-
-            <div class="form-group">
-                <label>Confirm New Password</label>
-                <input class="form-control" id="confirmPassword" name="confirmPassword" required>
-                <small id="confirmPasswordMessage" hidden></small> 
-            </div>
     </div>
             <input type="hidden" name="function" value="update">
             <input type="hidden" name="editUserId">
@@ -64,6 +45,44 @@
 </div> 
 </div>
 
+<!-- EDIT PASSWORD MODAL -->
+<div class="col-sm-2">
+    <button type="button" class="btn btn-success" data-toggle='modal' data-target='#editPasswordModal' onclick="editPasswordPrompt()">Edit Password</button>
+</div>
+
+<div class="modal fade" id="editPasswordModal" tabindex="-1" role="dialog" aria-labelledby="view-modal" aria-hidden="true">
+<div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title modal-title-custom ml-9 mr-auto text-black" id="editPasswordHead">Reset Password</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body" id="editPasswordBody">
+            <div class="wrapper">
+                <label>What is your current password?</label>
+                <input type="text" class="search-input form-control" id="passwordName" placeholder="Current Password" onkeyup="projectValidation()">
+                <div class="autocom-box"></div>
+            </div>
+            
+            <div class="wrapper">
+            <label>Password Update:</label>
+                <input type="text" class="search-input form-control" id="newpasswordName" placeholder="New Password" onkeyup="passwordValidation()">    
+            </div>
+
+            <div class="wrapper">
+            <label>Confirm Password Update:</label>
+                <input type="text" class="search-input form-control" id="confirmpasswordName" placeholder="Confirm New Password" onkeyup="passwordValidation()">    
+            </div>
+            <div class="password-modal-footer" id="editPasswordFooter">
+                <button class="btn btn-success" type="button" id="savePasswordBtn" onclick="postRequest()" data-toggle='modal' data-target='#passwordModal'>Update Password</button>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
 <div class="link">
     <p1>Linked With Github</p1>
     <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" style=”width:40px;height:20px;" class="icon">  
@@ -72,3 +91,4 @@
     <p2>Linked With Discord</p2>
     <img src="https://preview.redd.it/s9biyhs4lix61.jpg?auto=webp&s=c600a95eff95b3e9406a8b913c6aa3988b5e3a8b" style=”width:40px;height:20px;" class="icon">   
 </div>
+
