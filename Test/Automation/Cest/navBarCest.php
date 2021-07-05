0082716f6a74bd5b4505f4b9478048ba3899236c
MarcusBeddall <56123521+MarcusBeddall@users.noreply.github.com>
2021-07-05 18:45:52 +0100

    85 - updated navBarCest

--- Test/Automation/Cest/navBarCest.php
+++ Test/Automation/Cest/navBarCest.php
@@ -12,7 +12,25 @@ class navBarCest
     public function navigateToLoginPage(AcceptanceTester $I, Page\Acceptance\navBar $n)    
     {
         $n->toLoginPage();
-        $uri = $I->grabFromCurrentUrl();
-        $I->assertEquals($uri, "/login");
+        $url = $I->grabFromCurrentUrl();
+        $I->assertEquals("/login", $url);
     }
+
+    public function navigateToRegisterPage(AcceptanceTester $I, Page\Acceptance\navBar $n)    
+    {
+        $n->toRegisterPage();
+        $url = $I->grabFromCurrentUrl();
+        $I->assertEquals("/register", $url);
+    }
+
+    public function navigateToManagerPage(AcceptanceTester $I, Page\Acceptance\navBar $n, Page\Acceptance\Login $l)
+    {
+        $n->toLoginPage();
+        $l->login("od", "od");
+        $n->toManagerPage();
+        $url = $I->grabFromCurrentUrl();
+        $I->assertEquals("/manager", $url);
+    }  
+
+
 }
