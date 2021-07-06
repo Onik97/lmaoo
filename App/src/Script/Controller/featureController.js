70c254336eee5e9837d1d8c75fd013568572e018
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2021-07-06 13:18:05 +0100

    88 - code cleanup

--- App/src/Script/Controller/featureController.js
+++ App/src/Script/Controller/featureController.js
@@ -1,21 +1,18 @@
 import axios from "../Utility/AxiosWrapper.js";
 
-export default class Feature
-{
-    static async createFeature(json)
-    {
+export default class Feature {
+
+    static async createFeature(json) {
         let result = await axios.Post("/feature",json);
         return result;
     }
 
-    static async readFeature(featureId)
-    {
+    static async readFeature(featureId) {
         let result = await axios.Get(`/feature/${featureId}`);
         return result[0];
     }
 
-    static async updateFeature(json)
-    {
+    static async updateFeature(json) {
         let result = await axios.Put("/feature", json);
         return result;
     }
